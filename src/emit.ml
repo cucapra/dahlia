e06bba9c14480514824304e64afb2a12a98dcc8b
tedbauer <tjb272@cornell.edu>
2018-06-28 13:11:55 -0400

    Indentation and semicolon bugfix

--- src/emit.ml
+++ src/emit.ml
@@ -73,58 +73,60 @@ and emit_app (id, args) =
   concat [ id; "("; (emit_args args); ")" ]
 
 let rec emit_cmd i cmd =
-  (match cmd with
-  | CAssign (id, e)          -> emit_assign (id, e)
-  | CReassign (target, e)    -> emit_reassign (target, e)
+  match cmd with
+  | CAssign (id, e)          -> emit_assign (id, e) i
+  | CReassign (target, e)    -> emit_reassign (target, e) i
   | CFor (id, r1, r2, body)  -> emit_for (id, r1, r2, body) i
   | CIf (cond, body)         -> emit_if (cond, body) i
   | CSeq (c1, c2)            -> emit_seq (c1, c2) i
   | CFun (t, id, args, body) -> emit_fun (t, id, args, body) i
-  | CReturn e                -> emit_return e)
-  |> (indent i)
+  | CReturn e                -> emit_return e i
 
 and emit_assign_int (id, e) =
-  concat [ "int "; id; " = "; (emit_expr e) ]
+  concat [ "int "; id; " = "; (emit_expr e); ";" ]
 
 and emit_assign_arr (id, e, a) =
   let arr_size = string_of_int (Array.length a) in
   concat [
-    "int "; id; "["; arr_size; "]"
+    "int "; id; "["; arr_size; "]"; ";"
   ]
 
-and emit_assign (id, e) =
+and emit_assign (id, e) i =
   match !type_map id, e with
   | TInt _, _
-  | TBool, _                         -> emit_assign_int (id, e)
-  | TArray (t, bf), EArray (_, _, a) -> emit_assign_arr (id, e, a)
+  | TBool, _                         -> emit_assign_int (id, e)    |> indent i
+  | TArray (t, bf), EArray (_, _, a) -> emit_assign_arr (id, e, a) |> indent i
 
-and emit_reassign (target, e) =
-  concat [ (emit_expr target); " = "; (emit_expr e) ]
+and emit_reassign (target, e) i =
+  concat [ (emit_expr target); " = "; (emit_expr e); ";" ] |> indent i
 
 and emit_for (id, r1, r2, body) i =
   concat [ 
-    "for (int "; id; " = "; (emit_expr r1); ";"; id;
-    " <= "; (emit_expr r2); " "; id; " += 1) {"; newline
+    "for (int "; id; " = "; (emit_expr r1); "; "; id;
+    " <= "; (emit_expr r2); "; "; id; " += 1) {"; newline
   ] 
   |> fun s -> concat [ s; (emit_cmd (i+1) body); newline; "}" ]
+  |> indent i
 
 and emit_if (cond, body) i =
   concat [ 
     "if ("; (emit_expr cond); ") {"; newline; (emit_cmd (i+1) body);
     newline; "}"
   ]
+  |> indent i
 
 and emit_seq (c1, c2) i =
-  concat [ (emit_cmd i c1); newline; (emit_cmd i c2) ]
+  concat [ (emit_cmd i c1); newline; (emit_cmd i c2); ]
 
 and emit_fun (t, id, args, body) i =
   concat [ 
     (type_str t); " "; id; "("; (emit_args args); ") {";
     newline; (emit_cmd (i+1) body); newline; "}"
   ]
+  |> indent i
 
-and emit_return e =
-  concat [ "return "; (emit_expr e) ]
+and emit_return e i =
+  concat [ "return "; (emit_expr e); ";" ] |> indent i
 
 and generate_c cmd =
   emit_cmd 0 cmd
