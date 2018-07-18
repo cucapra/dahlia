a7cb5f075a1abdedd3fc213139627b77ee3b8315
tedbauer <tjb272@cornell.edu>
2018-07-18 16:37:01 -0400

    Edit AST node for logical access to allow any number of bracket pairs; add some documentation for AST

--- src/ast.ml
+++ src/ast.ml
@@ -1,11 +1,28 @@
 type id = string
 
+(* A [type_node] is one of the following: 
+     - [TInt i]: an integer type, where if [i] is [Some i'], it's a 
+       static integer with value i', and if [i] is [None], it's non-static
+     - [TBool]: a boolean type
+     - [TArray (t, bf, s)]: an array type with array element type [t], 
+       banking factor [bf], and size [s]
+     - [TIndex (s, d)]: an index type where [s] is its static component, 
+       a list of integers representing the indices it itself simultaneously
+       represents; and where [d] is its dynamic component, which is [None] 
+       if it has no dynamic component, and [Some s] where s is the maximum 
+       value it can dynamically represent
+     - [TFunc t]: a function type where [t] is a list of the types of its 
+       arguments (the order of the list corresponds to the order of the 
+       arguments)
+     - [TAlias i]: an alias type where [i] is an id; under a typing context 
+       [c] this may or may not map to another [type_node]
+     - [TFloat]: a float type *)       
 type type_node =
-  | TInt of int option (* Some i => static int with value i *)
+  | TInt of int option 
   | TBool
-  | TArray of type_node * int * int (* (type, banking factor, size) *)
-  | TIndex of int list * int option (* (static comp, dynamic comp) *)
-  | TFunc of type_node list (* list of arg types *)
+  | TArray of type_node * int * int 
+  | TIndex of int list * int option 
+  | TFunc of type_node list 
   | TAlias of id
   | TFloat
 
@@ -30,8 +47,8 @@ type expression =
   | EBool of bool
   | EBinop of binop * expression * expression
   | EArray of type_node * int * expression array
-  | EArrayExplAccess of id * expression * expression
-  | EArrayImplAccess of id * expression 
+  | EPhysAccess of id * expression * expression
+  | ELoglAccess of id * expression list
 
 type value =
   | VInt of int
