390cbd251b0ab9f3d3c3ad5014250ae15a8e5257
tedbauer <tjb272@cornell.edu>
2018-07-25 14:01:08 -0400

    Add some more error message

--- src/error_msg.ml
+++ src/error_msg.ml
@@ -14,14 +14,53 @@ let rec string_of_type = function
   | TFloat -> "float"
   | TFunc _ -> "func"
 
+let string_of_binop = function
+  | BopEq    -> "="
+  | BopNeq   -> "!="
+  | BopGeq   -> ">="
+  | BopLeq   -> "<="
+  | BopLt    -> "<"
+  | BopGt    -> ">"
+  | BopPlus  -> "+"
+  | BopMinus -> "-"
+  | BopTimes -> "*"
+  | BopOr    -> "||"
+  | BopAnd   -> "&&"
+
 let illegal_bank i id =
-  "Illegal bank access on array " ^ id ^ ": " ^ string_of_int i
+  "Type error: Illegal bank access on array " ^ id ^ ": " ^ string_of_int i
 
 let range_error =
-  "Range start/end must be integers"
+  "Type error: Range start/end must be integers"
 
 let unexpected_type id actual expected =
-  "Type Error: " ^ id ^
+  "Type error: " ^ id ^
   " was of type " ^ (string_of_type actual) ^
-  " but type " ^ expected ^ " was expected"
+  " but type " ^ (string_of_type expected) ^ " was expected"
+
+let illegal_accessor_type t id =
+  "Type error: can't access array " ^ id ^ " with type " ^ (string_of_type t)
+
+let illegal_access id =
+  "Type error: can't index into non-array " ^ id
+
+let illegal_op binop t1 t2 =
+  "Type error: can't apply operator '" ^
+  (string_of_binop binop) ^ 
+  "' to " ^ 
+  (string_of_type t1) ^ 
+  " and " ^ 
+  (string_of_type t2)
+
+let small_mux =
+  "Illegal access operation: mux is smaller than array banking factor"
+
+let illegal_app id =
+  id ^ " is not a function and cannot be applied"
+
+let illegal_mux =
+  "Type error: can't use multiplexer to access non-array"
+
+let static_bank_error =
+  "Bank accessor must be static"
 
