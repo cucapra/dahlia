8dd36618345e6d278e6d254de4c29381dc0edcf3
Adrian Sampson <adrian@radbox.org>
2018-08-02 18:28:59 -0400

    `work` context includes directory change

--- buildbot/buildbot/worker.py
+++ buildbot/buildbot/worker.py
@@ -19,13 +19,28 @@ class WorkError(Exception):
 
 
 @contextmanager
+def chdir(path):
+    """Temporarily change the working directory (then change back).
+    """
+    old_dir = os.getcwd()
+    os.chdir(path)
+    yield
+    os.chdir(old_dir)
+
+
+@contextmanager
 def work(db, old_state, temp_state, done_state):
     """A context manager for acquiring a job temporarily in an
     exclusive way to work on it.
+
+    While inside the context, the working directory is changed to the
+    job's private work directory.
     """
     job = db.acquire(old_state, temp_state)
+    job_dir = db.job_dir(job['name'])
     try:
-        yield job
+        with chdir(job_dir):
+            yield job
     except WorkError as exc:
         log(job, exc.message)
         db.set_state(job, 'failed')
@@ -140,7 +155,6 @@ def stage_unpack(db, config):
         runl(
             job,
             ["unzip", "-d", CODE_DIR, "{}.zip".format(ARCHIVE_NAME)],
-            cwd=db.job_dir(job['name']),
         )
 
 
@@ -182,11 +196,9 @@ def stage_hls(db, config):
     """
     prefix = config["HLS_COMMAND_PREFIX"]
     with work(db, 'seashelled', 'hlsing', 'hlsed') as job:
-        code_dir = os.path.join(db.job_dir(job['name']), CODE_DIR)
-        c_main = job['c_main']
-
         # Run the Xilinx SDSoC compiler.
-        runl(job, prefix + ['sds++', '-c', c_main], cwd=code_dir)
+        c_main = job['c_main']
+        runl(job, prefix + ['sds++', '-c', c_main], cwd=CODE_DIR)
 
 
 def work_threads(db, config):
