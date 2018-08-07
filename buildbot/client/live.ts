8a0b46ca405fa552a922cbf4df69c30b66dfe9f3
Adrian Sampson <adrian@radbox.org>
2018-08-07 19:16:22 -0400

    Poll jobs

--- buildbot/client/live.ts
+++ buildbot/client/live.ts
@@ -11,13 +11,27 @@ async function startJob(code: string): Promise<string> {
 document.addEventListener("DOMContentLoaded", () => {
   const textarea = document.querySelector("textarea");
   const button = document.querySelector("button");
+  const result = document.querySelector("#result");
 
   async function compile() {
     const code = textarea.value;
     console.log("compiling", code);
 
     const jobName = await startJob(code);
-    console.log(jobName);
+    console.log("started job", jobName);
+    result.textContent = 'job started...';
+
+    const poll = async () => {
+      const res = await fetch("/jobs/" + jobName);
+      const job = await res.json();
+      console.log("updated job", job);
+
+      if (job.state === "failed") {
+        result.textContent = 'failed!';
+      }
+    };
+
+    setTimeout(poll, 100);
   }
 
   button.addEventListener("click", compile);
