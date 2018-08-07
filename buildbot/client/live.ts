7a3606e4ed1d542108b60accf276188b52e75185
Adrian Sampson <adrian@radbox.org>
2018-08-07 19:09:08 -0400

    Actually kick off jobs

--- buildbot/client/live.ts
+++ buildbot/client/live.ts
@@ -1,1 +1,24 @@
-console.log("hi");
+async function startJob(code: string): Promise<string> {
+  const data = new FormData();
+  data.append('code', code);
+  const res = await fetch("/jobs", {
+      method: "POST",
+      body: data,
+  });
+  return await res.text();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const textarea = document.querySelector("textarea");
+  const button = document.querySelector("button");
+
+  async function compile() {
+    const code = textarea.value;
+    console.log("compiling", code);
+
+    const jobName = await startJob(code);
+    console.log(jobName);
+  }
+
+  button.addEventListener("click", compile);
+});
