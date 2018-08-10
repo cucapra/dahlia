f70a10a9d4e8e65b94b09cbe6acfc30b8d7db06b
Adrian Sampson <adrian@radbox.org>
2018-08-10 17:12:19 -0400

    Move text extensions to config

--- buildbot/buildbot/config_default.py
+++ buildbot/buildbot/config_default.py
@@ -1,3 +1,17 @@
 UPLOAD_EXTENSIONS = ['zip']
 SEASHELL_COMPILER = 'seac'
 HLS_COMMAND_PREFIX = []
+
+# Filename extensions to send as plain text for job file viewing.
+TEXT_EXTENSIONS = [
+    'ss',
+    'sea'
+    'c',
+    'cpp',
+    's',
+    'log',
+    'jou',
+    'tcl',
+    'd',
+    'dat',
+]
