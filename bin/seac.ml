9d31176122792f043ada2b42591ed8feb0476c14
Rachit Nigam <rachit.nigam12@gmail.com>
2018-09-06 20:31:59 -0400

    Give proper parser errors

--- bin/seac.ml
+++ bin/seac.ml
@@ -3,6 +3,26 @@ open Lexer
 open Lexing
 open Context
 
+let print_position outx lexbuf =
+  let pos = lexbuf.lex_curr_p in
+  Core.fprintf outx "line %d, column %d"
+    pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)
+
+let parse_with_error lexbuf =
+  try Some (Parser.prog Lexer.token lexbuf) with
+  | SyntaxError msg ->
+    Core.fprintf stderr "%a: %s\n" print_position lexbuf msg;
+    None
+  | Parser.Error ->
+    Core.fprintf stderr "Syntax error on %a\n" print_position lexbuf;
+    exit (-1)
+
+let typecheck_with_error (ast : Ast.command) : (gamma * delta) option =
+  try
+    Some (Type.check_cmd ast (Context.empty_gamma, Context.empty_delta))
+  with
+    Type.TypeError s -> print_endline s; None
+
 let no_typecheck = ref false
 
 let usage = "usage: " ^ Sys.argv.(0) ^ "[-nt]"
@@ -11,6 +31,10 @@ let specs = [
   ("-nt", Arg.Set no_typecheck, ": turn off typechecking")
 ]
 
+let ( >>= ) opt f = match opt with
+| Some v -> f v
+| None -> None
+
 let _ =
 
   Arg.parse
@@ -18,18 +42,10 @@ let _ =
     (fun s -> raise (Arg.Bad ("Invalid argument " ^ s)))
     usage;
 
-  let lexbuf = Lexing.from_channel stdin in
-  let commands = Parser.prog Lexer.token lexbuf in
-  try
-    if not !no_typecheck then
-      let (ctx, dta) =
-        Type.check_cmd commands (Context.empty_gamma, Context.empty_delta)
-      in
-      Emit.set_type_map (fun id -> Context.get_binding id ctx);
-      Emit.set_delta_map (fun id -> Context.get_alias_binding id dta)
-    else ();
-
-    print_endline (Emit.generate_c commands)
-
-  with
-    Type.TypeError s -> print_endline s
+  let lexbuf = Lexing.from_channel Core.In_channel.stdin in
+  parse_with_error lexbuf  >>= fun ast ->
+  typecheck_with_error ast >>= fun (ctx, dta) ->
+    Emit.set_type_map (fun id -> Context.get_binding id ctx);
+    Emit.set_delta_map (fun id -> Context.get_alias_binding id dta);
+    print_endline (Emit.generate_c ast);
+    None
--- src/lexer.mll
+++ src/lexer.mll
@@ -1,22 +1,30 @@
 {
+open Lexing
 open Parser
+
+exception SyntaxError of string
+
+let next_line lexbuf =
+  let pos = lexbuf.lex_curr_p in
+  lexbuf.lex_curr_p <-
+    { pos with pos_bol = lexbuf.lex_curr_pos;
+               pos_lnum = pos.pos_lnum + 1
+    }
 }
 
 let num = '-'? ['0'-'9'] ['0'-'9']*
-
 let frac = '.' ['0'-'9']+
 let fl = '-'? ['0'-'9'] frac?
-
 let id = ['a'-'z' 'A'-'Z' '_'] ['a'-'z' 'A'-'Z' '_' '0'-'9']*
-
-let ws = ['\t' ' ' '\n' '\r']
-
+let white = [' ' '\t']+
+let newline = '\r' | '\n' | "\r\n"
 let comment = "//" [^ '\r' '\n']* ['\n' '\r']
 
 rule token =
   parse
-  | ws+          { token lexbuf }
-  | comment      { token lexbuf }
+  | white        { token lexbuf }
+  | comment      { next_line lexbuf; token lexbuf}
+  | newline      { next_line lexbuf; token lexbuf}
 
   | num as n     { INT (int_of_string n) }
   | fl as f      { FLOAT (float_of_string f) }
@@ -68,4 +76,5 @@ rule token =
 
   | id as i      { ID (i) }
 
+  | _            { raise (SyntaxError ("Unexpected char: " ^ Lexing.lexeme lexbuf)) }
   | eof          { EOF }
