2f33afb42e8c6e4f84c433cc401865fdb9877bdb
Adrian Sampson <adrian@radbox.org>
2018-09-18 16:46:57 -0400

    Refactor as a class

--- buildbot/buildbot/workproc.py
+++ buildbot/buildbot/workproc.py
@@ -2,22 +2,25 @@ import curio
 import os
 
 
-async def handle(client, addr):
-    print('connected')
-    while True:
-        async for line in client.makefile('rb'):
-            print(line)
-    print('closed')
+class WorkProc:
+    async def handle(self, client, addr):
+        """Handle an incoming socket connection.
+        """
+        while True:
+            async for line in client.makefile('rb'):
+                print(line)
 
-
-def workproc(sockpath='workproc.sock'):
-    try:
-        curio.run(curio.unix_server, sockpath, handle)
-    except KeyboardInterrupt:
-        pass
-    finally:
-        os.unlink(sockpath)
+    def serve(self, sockpath='workproc.sock'):
+        """Start listening on a Unix domain socket for incoming
+        messages. Run indefinitely (until the server is interrupted).
+        """
+        try:
+            curio.run(curio.unix_server, sockpath, self.handle)
+        except KeyboardInterrupt:
+            pass
+        finally:
+            os.unlink(sockpath)
 
 
 if __name__ == '__main__':
-    workproc()
+    WorkProc().serve()
