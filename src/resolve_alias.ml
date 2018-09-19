eeaf653768c8ead3b05b3d952e8a2832e9228633
Rachit Nigam <rachit.nigam12@gmail.com>
2018-09-19 19:16:36 -0400

    Implement pass to infer capabilities map

--- src/resolve_alias.ml
+++ src/resolve_alias.ml
@@ -13,7 +13,6 @@ let get_alias_binding id d =
   try StringMap.find id d
   with Not_found -> raise (Context.NoBinding id)
 
-<<<<<<< HEAD
 class type_alias_resolver = object
   inherit [delta] ast_mapper
 
@@ -26,42 +25,7 @@ class type_alias_resolver = object
 
   (** Don't visit expressions *)
   method! private expr e st = e, st
-
 end
 
 let remove_aliases cmd =
   (new type_alias_resolver)#command cmd StringMap.empty |> fst
-=======
-let rec resolve_type typ dta = match typ with
-  | TBool | TFloat | TMux _ | TIndex _ -> typ
-  | TAlias id -> get_alias_binding id dta
-  | TLin t -> TLin (resolve_type t dta)
-  | TArray (t, ts) -> TArray (resolve_type t dta, ts)
-  | TFunc tlist -> TFunc (List.map (fun t -> resolve_type t dta) tlist)
-
-let rec resolve_cmd_seq clst dta = match clst with
-  | [] -> ([], dta)
-  | cmd :: tl ->
-      let (c1, dta1) = resolve_cmd cmd dta in
-      let (ctl, dta2) = resolve_cmd_seq tl dta1 in
-      c1 :: ctl, dta2
-
-and resolve_cmd cmd dta : command * delta = match cmd with
-  | CMuxDef _ | CCap _ | CAssign _ | CReassign _ | CApp _ | CExpr _ -> cmd, dta
-  | CIf (e, c) ->
-      let (c1, dta1) = resolve_cmd c dta in
-      CIf (e, c1), dta1
-  | CFor (id, e1, e2, un, c) ->
-      let (c1, dta1) = resolve_cmd c dta in
-      CFor (id, e1, e2, un, c1), dta1
-  | CSeq clst ->
-      let (clst1, dta1) = resolve_cmd_seq clst dta in
-      CSeq clst1, dta1
-  | CFuncDef (id, id_typ_lst, c) ->
-      let id_typ_lst1 = List.map (fun (i, t) -> i, resolve_type t dta) id_typ_lst in
-      let (c1, dta1) = resolve_cmd c dta in
-      CFuncDef(id, id_typ_lst1, c1), dta1
-  | CTypeDef (id, typ) -> cmd, add_alias_binding id (resolve_type typ dta) dta
-
-let remove_aliases cmd = fst @@ resolve_cmd cmd StringMap.empty
->>>>>>> Implement TLin
