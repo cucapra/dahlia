2dd18a3a1723a19b333aaf773aadbfa18dd8029b
Rachit Nigam <rachit.nigam12@gmail.com>
2018-09-27 17:20:10 -0400

    refactoring for testing

--- bin/seac.ml
+++ bin/seac.ml
@@ -3,35 +3,14 @@ open Cmdliner
 open Seashell
 open Compile_utils
 
-let seac filename no_typecheck print_ast : unit =
+let seac filename print_ast : unit =
   Printexc.record_backtrace false;
   let prog = In_channel.read_all filename in
-  let ast = parse_with_error prog in
-  begin if print_ast then
-    print_endline ("=======Parsed AST=========\n" ^ Ast.show_command ast)
-  end;
-  let rast = Resolve_alias.remove_aliases ast in
-  begin if print_ast then
-    print_endline ("=======Resolved AST=========\n" ^ Ast.show_command rast)
-  end;
-  let ctx = begin
-    if not no_typecheck then
-      typecheck_with_error rast
-    else
-      Context.empty_gamma
-  end in
-  begin if print_ast then
-    print_endline "==========================="
-  end;
-  emit_code rast ctx
+  compile_string prog print_ast
 
 let filename =
   let doc = "The file to be compiler by the seashell compiler." in
   Arg.(required & pos 0 (some file) None & info [] ~docv:"FILENAME" ~doc)
-
-let no_typecheck =
-  let doc = "Emit code without type checking." in
-  Arg.(value & flag & info ["nt"; "no-typecheck"] ~doc)
 
 let print_ast =
   let doc = "Print the various intermediate states of the AST." in
@@ -41,6 +20,6 @@ let info =
   let doc = "The Seashell compiler" in
   Term.info "seac" ~exits:Term.default_exits ~doc
 
-let seac_t = Term.(const seac $ filename $ no_typecheck $ print_ast)
+let seac_t = Term.(const seac $ filename $ print_ast)
 
 let () = Term.exit @@ Term.eval (seac_t, info)
--- test/test_utils.ml
+++ test/test_utils.ml
@@ -1,13 +1,7 @@
 open Core
 open Seashell
-open Compile_utils
 
-let compile_string prog =
-  Printexc.record_backtrace false;
-  let ast = parse_with_error prog in
-  let rast = Resolve_alias.remove_aliases ast in
-  let ctx = typecheck_with_error rast in
-  emit_code rast ctx
+let compile_string prog = Compile_utils.compile_string prog false
 
 let compile filename =
   let prog = In_channel.read_all filename in
