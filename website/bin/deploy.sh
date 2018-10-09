3e060692c01d54f81b4af2da228a9739a53333d9
Rachit Nigam <rachit.nigam12@gmail.com>
2018-10-09 17:34:09 -0400

    rysnc is magik

--- website/bin/deploy.sh
+++ website/bin/deploy.sh
@@ -3,7 +3,7 @@
 set -x
 set -e
 
-DEST=courses:coursewww/capra.cs.cornell.edu/htdocs/seashell
+DEST=courses:coursewww/capra.cs.cornell.edu/htdocs/seashell/
 
 rsync --compress --recursive --checksum --itemize-changes \
 	--delete -e ssh --perms --chmod=Du=rwx,Dgo=rx,Fu=rw,Fog=r \
