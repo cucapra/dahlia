a024411fe45fd088ed8056ee0518ad9532e4122a
Rachit Nigam <rachit.nigam12@gmail.com>
2018-10-09 17:19:53 -0400

    fix demo deploy script

--- website/bin/deploy.sh
+++ website/bin/deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-RSYNCARGS="--compress --recursive --checksum --itemize-changes \
-	--delete -e ssh --perms --chmod=Du=rwx,Dgo=rx,Fu=rw,Fog=r \
-	--delete-excluded \
-	--exclude .DS_Store --exclude Makefile"
 DEST=courses:coursewww/capra.cs.cornell.edu/htdocs/seashell
 
-rsync $(RSYNCARGS) ../dist/ $(DEST)
+rsync --compress --recursive --checksum --itemize-changes \
+	--delete -e ssh --perms --chmod=Du=rwx,Dgo=rx,Fu=rw,Fog=r \
+	--delete-excluded \
+	--exclude .DS_Store ./dist $(DEST)
