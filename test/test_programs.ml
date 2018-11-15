5f60b070d3b4395443f5a1afcf535559627ec8e4
tedbauer <tjb272@cornell.edu>
2018-11-15 03:57:31 -0500

    Add a simple test case

--- test/test_programs.ml
+++ test/test_programs.ml
@@ -130,6 +130,20 @@ let%expect_test "should-compile/vsadd_nrl_typedefs.sea" =
       }
     } |}]
 
+;;
+
+let%expect_test "should-compile/simpleview.sea" =
+  compile "should-compile/simpleview.sea";
+  [%expect {|
+    #include "apcint.h"
+    void test_view(uint10 a[8]) {
+      #pragma HLS ARRAY_PARTITION variable=a factor=2
+      for (int j = 0; j <= 2; j += 1) {
+        /* cap write: a[1*(0+j*1)] */
+        a[1*(0+j*1)] = 1;
+      }
+    } |}]
+
 (** TODO(rachit): These tests need + on idx types to be implemented
 
 let%expect_test "should-compile/logical_access.sea" =
