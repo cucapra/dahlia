4eccabda58263f99d3df8b7954e094e2124766bb
Rachit Nigam <rachit.nigam12@gmail.com>
2019-01-30 14:26:25 -0500

    simple C pretty printer

---
+++ src/main/scala/Emit.scala
@@ -0,0 +1,55 @@
+package fuselang
+
+import org.bitbucket.inkytonik.kiama.output._
+
+// TODO(rachit): Make implicits private.
+
+object CCodeGen extends PrettyPrinter {
+
+  import scala.language.implicitConversions
+  import Syntax._
+  import TypeEnv.Env
+
+  override val defaultIndent = 2
+
+  def scope(doc: Doc): Doc =
+    lbrace <@> indent(doc) <@> rbrace
+
+  implicit def op2ToDoc(op: Op2): Doc = op match {
+    case OpEq => "="
+    case OpAdd => "+"
+  }
+
+  implicit def typeToDoc(typ: Type): Doc = typ match {
+    case TBool => "int"
+    case TSizedInt(_) => value(typ)
+    case TArray(typ, dims) => typ <> brackets(value(dims.map(_._1).foldLeft(1)(_ * _)))
+    case TIndex(_, _) => "int"
+  }
+
+  implicit def exprToDoc(e: Expr): Doc = e match {
+    case EInt(i) => value (i)
+    case EBool(b) => value(if(b) 1 else 0)
+    case EVar(id) => value(id)
+    case EBinop(op, e1, e2) => e1 <+> op <+> e2
+    case EAA(id, idxs) => id <> hcat(idxs.map(idx => brackets(idx)))
+  }
+
+  implicit def cmdToDoc(c: Command)(implicit env: Env): Doc = c match {
+    case CSeq(c1, c2) => c1 <> semi <> line <> c2 <> semi
+    case CLet(id, e) => env(id).typ <+> value(id) <+> equal <+> e <> semi
+    case CIf(cond, cons) => "if" <> parens(cond) <> scope (cons)
+    case CFor(iter, range, par) =>
+      "for" <> parens {
+        env(iter).typ <+> iter <+> "=" <+> value(range.s) <> semi <+>
+        iter <+> "<" <+> value(range.e) <> semi <+>
+        iter <+> "++"
+      } <+> scope(par)
+    case CUpdate(lhs, rhs) => lhs <+> "=" <+> rhs <> semi
+    case CExpr(e) => e <> semi
+    case CEmpty => ""
+  }
+
+  def pretty(f: Fuse)(implicit env: Env) = vsep(f.prog.map(cmdToDoc), semi)
+
+}
