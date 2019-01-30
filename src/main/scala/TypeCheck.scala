4d7c446fbe3b9f3c5124163b55cc3e314483292b
Rachit Nigam <rachit.nigam12@gmail.com>
2019-01-30 12:14:19 -0500

    dumb type checker finished

--- src/main/scala/TypeCheck.scala
+++ src/main/scala/TypeCheck.scala
@@ -1,22 +1,11 @@
 package fuselang
 
+import scala.math.max
+
 import Syntax._
+import TypeErrors._
 
-object TypeErrors {
-
-  sealed trait TypeError extends RuntimeException
-  case class MsgError(msg: String) extends TypeError
-  case class UnexpectedType(construct: String, exp: Type, actual: Type) extends TypeError {
-    override def toString = s"Expected type $exp in $construct, received: $actual."
-  }
-  case class UnexpectedSubtype(construct: String, exp: Type, actual: Type) extends TypeError {
-    override def toString = s"Expected subtype of $exp in $construct, received: $actual."
-  }
-}
-
-object TypeChecker {
-  import TypeErrors._
-
+object TypeEnv {
   type Env = Map[Id, Info]
 
   implicit class RichCheck(checkRet: (Type, Env)) {
@@ -29,11 +18,11 @@ object TypeChecker {
     }
   }
 
-  class Info(id: Id, typ: Type, avBanks: Map[Int, Set[Int]], conBanks: Map[Int, Set[Int]]) {
+  case class Info(id: Id, typ: Type, avBanks: Map[Int, Set[Int]], conBanks: Map[Int, Set[Int]]) {
     def consumeBank(dim: Int, bank: Int): Either[TypeError, Info] = avBanks.contains(dim) match {
       case true => if (avBanks(dim).contains(bank)) {
         Right(
-          new Info(
+          Info(
             id,
             typ,
             avBanks + (dim -> (avBanks(dim) - bank)),
@@ -48,25 +37,53 @@ object TypeChecker {
   }
 
   object Info {
-    def apply(id: Id, typ: Type) = typ match {
+    def apply(id: Id, typ: Type): Info = typ match {
       case TArray(_, dims) => {
         val banks = dims.map({case (_, b) => b})
-        new Info(
+        Info(
           id,
           typ,
           banks.zipWithIndex.map({case (i, banks) => i -> 0.until(banks).toSet}).toMap,
           banks.zipWithIndex.map({case (i, _) => i -> Set[Int]()}).toMap)
       }
-      case _ => new Info(id, typ, Map(), Map())
+      case _ => Info(id, typ, Map(), Map())
+    }
+  }
+}
+
+object TypeChecker {
+  import TypeEnv._
+
+  def checkFuse(f: Fuse) = f.prog.foldLeft(Map[Id, Info]())({
+    case (env, cmd) => checkC(cmd)(env)
+  })
+
+  private def checkB(t1: Type, t2: Type, b: Op2) = b match {
+    case OpEq => {
+      if (t1 :< t2 || t2 :< t1) TBool
+      else throw UnexpectedSubtype("=", t1, t2)
+    }
+    case OpAdd => (t1, t2) match {
+      case (TSizedInt(s1), TSizedInt(s2)) => TSizedInt(max(s1, s2))
+      case (TSizedInt(_), _) => throw UnexpectedSubtype("+", t1, t2)
+      case (_, TSizedInt(_)) => throw UnexpectedSubtype("+", t2, t1)
+      case (_, _) => throw UnexpectedSubtype("+", TSizedInt(64), t1)
     }
   }
 
-  def checkE(expr: Expr)(implicit env: Env): (Type, Env) = expr match {
+  private def checkE(expr: Expr)(implicit env: Env): (Type, Env) = expr match {
     case EInt(_) => TSizedInt(64) -> env
+    case EBool(_) => TBool -> env
+    case EVar(id) => env(id).typ -> env
+    case EBinop(op, e1, e2) => {
+      val (t1, env1) = checkE(e1)
+      val (t2, env2) = checkE(e2)(env1)
+      checkB(t1, t2, op) -> env2
+    }
     case _ => TBool -> env
   }
 
-  def checkC(cmd: Command)(implicit env: Env): Env = cmd match {
+  private def checkC(cmd: Command)(implicit env: Env): Env = cmd match {
     case CSeq(c1, c2) => checkC(c2)(checkC(c1))
     case CIf(cond, cons) => checkE(cond).typRun(TBool, "if condition", env => checkC(cons)(env))
     case CUpdate(lhs, rhs) => {
@@ -79,8 +96,10 @@ object TypeChecker {
       val (t, e1) = checkE(exp)
       e1 + (id -> Info(id, t))
     }
+    case CFor(iter, range, par) => {
+      checkC(par)(env + (iter -> Info(iter, range.idxType)))
+    }
     case CExpr(e) => checkE(e)._2
     case CEmpty => env
   }
-
 }
