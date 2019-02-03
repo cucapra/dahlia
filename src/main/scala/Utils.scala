dfeaef2f8a1c20d64a7eb54ec6a022566904df87
Rachit Nigam <rachit.nigam12@gmail.com>
2019-02-02 23:42:55 -0500

    stringToId for tests

--- src/main/scala/Utils.scala
+++ src/main/scala/Utils.scala
@@ -2,6 +2,13 @@ package fuselang
 
 object Utils {
 
+  import scala.language.implicitConversions
+
+  // Allow for env("x") style calls.
+  implicit def stringToId(s: String) = {
+    Syntax.Id(s)
+  }
+
   def parseAst(s: String) = FuseParser.parse(s)
   def typeCheck(s: String) = TypeChecker.checkFuse(FuseParser.parse(s))
 
