06a28bb4d29c88599ed89484eb5bf920e06e09e4
Adrian Sampson <adrian@radbox.org>
2019-02-05 21:04:53 -0500

    Simplify the argument parsing (#48)
    Don't require `-f` to pass the file to parse; it's now a positional
    argument.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,26 +1,26 @@
 package fuselang
 
-import java.nio.file.{Files, Paths}
+import java.nio.file.Files
+import java.io.File
 
 object Main {
-  case class Config(filename: String)
+  case class Config(srcfile: File)
 
   val parser = new scopt.OptionParser[Config]("fuse"){
 
     head("fuse", "0.0.1")
 
-    opt[String]('f', "srcFile")
+    arg[File]("<srcfile>")
       .required()
-      .valueName("<file>")
-      .action((x, c) => c.copy(filename = x))
-      .text("srcFile containing fuse source code")
+      .action((x, c) => c.copy(srcfile = x))
+      .text("source code file")
   }
 
   def main(args: Array[String]) = {
 
-    parser.parse(args, Config("")) match {
+    parser.parse(args, Config(null)) match {
       case Some(c) => {
-        val prog = new String(Files.readAllBytes(Paths.get(c.filename)))
+        val prog = new String(Files.readAllBytes(c.srcfile.toPath()))
         println(Compiler.compileString(prog))
       }
       case None => {
