2b300638d163fa7670adc5ce56297b89e04323a2
Rachit Nigam <rachit.nigam12@gmail.com>
2019-02-09 19:07:39 -0500

    pseudocode for array access checking and capabilities scaffolding

--- src/main/scala/TypeCheck.scala
+++ src/main/scala/TypeCheck.scala
@@ -15,6 +15,24 @@ import Errors._
  * - CLet with an explicit type get the correct type
  * - Binders for id update the `typ` var defined on the Id class.
  */
+
+/**
+ * Type checking array accesses is the most important and complex step.
+ * Here is the pseudocode for the algorithm:
+ *
+ *  function consume_banks(expr, env):
+ *    consume banks and return product of static parts of index types of accessors.
+ *
+ *  function check_eaa(env, cap_env, req_resources, required_cap, expr):
+ *    if cap_env(expr) == R and required_cap == W: FAIL
+ *    else if cap_env(expr) == R: SUCCESS
+ *    else if cap_env(expr) == W: FAIL
+ *    else:
+ *      n = consume_banks(expr, env)
+ *      if required_cap == W and req_resources != n: FAIL
+ *      cap_env(expr).bind(req_resources)
+ *      SUCCESS
+ */
 object TypeChecker {
   import TypeEnv._
 
@@ -27,7 +45,11 @@ object TypeChecker {
       id.typ = Some(typ);
       env.add(id -> Info(id, typ))
     })
-    checkC(p.cmd)(initEnv, List[Int]())
+    checkC(p.cmd)(initEnv, 1)
+  }
+
+  private def checkLVal(e: Expr)(implicit env: Env, rreq: ReqResources) = e match {
+    case _ => ???
   }
 
   private def checkB(t1: Type, t2: Type, op: BOp) = op match {
@@ -47,12 +69,11 @@ object TypeChecker {
       case (_: TFloat, _: TFloat) => TFloat()
       case _ => throw BinopError(op, t1, t2)
     }
-
   }
 
   // Implicit parameters can be elided when a recursive call is reusing the same env and its.
   // See EBinop an example.
-  private def checkE(expr: Expr)(implicit env: Env, its: ItStack): (Type, Env) = expr match {
+  private def checkE(expr: Expr)(implicit env: Env, rres: ReqResources): (Type, Env) = expr match {
     case EFloat(_) => TFloat() -> env
     case EInt(v) => TStaticInt(v) -> env
     case EBool(_) => TBool() -> env
@@ -63,7 +84,7 @@ object TypeChecker {
     }
     case EBinop(op, e1, e2) => {
       val (t1, env1) = checkE(e1)
-      val (t2, env2) = checkE(e2)(env1, its)
+      val (t2, env2) = checkE(e2)(env1, rres)
       checkB(t1, t2, op) -> env2
     }
     case EAA(id, idxs) => env(id).typ match {
@@ -77,7 +98,7 @@ object TypeChecker {
         // used up by consumed bank.
         val (e1, bres) = idxs.zipWithIndex.foldLeft((env, 1))({
           case ((env1, bres), (e, i)) =>
-            checkE(e)(env1, its) match {
+            checkE(e)(env1, rres) match {
               case (TIndex((s, e), _), env2) =>
                 env2.update(id -> env2(id).consumeDim(i, e - s)) -> bres * (e - s)
               case (TStaticInt(v), env2) =>
@@ -85,10 +106,9 @@ object TypeChecker {
               case (t, _) => throw InvalidIndex(id, t)
             }
         })
-        val reqRes = its.foldLeft(1)(_ * _)
-        if (reqRes != bres) {
+        if (rres != bres) {
           throw MsgError(
-            s"Invalid resources provided by array access in this context. Expected $reqRes, received: $bres")
+            s"Invalid resources provided by array access in this context. Expected $rres, received: $bres")
         }
         typ -> e1
       }
@@ -96,19 +116,19 @@ object TypeChecker {
     }
   }
 
-  private def checkC(cmd: Command)(implicit env: Env, its: ItStack): Env = cmd match {
-    case CPar(c1, c2) => checkC(c2)(checkC(c1), its)
+  private def checkC(cmd: Command)(implicit env: Env, rres: ReqResources): Env = cmd match {
+    case CPar(c1, c2) => checkC(c2)(checkC(c1), rres)
     case CIf(cond, cons) => {
-      val (cTyp, e1) = checkE(cond)(env.addScope, its)
+      val (cTyp, e1) = checkE(cond)(env.addScope, rres)
       if (cTyp != TBool()) {
         throw UnexpectedType(cond.pos, "if condition", TBool().toString, cTyp)
       } else {
-        checkC(cons)(e1, its).endScope._1
+        checkC(cons)(e1, rres).endScope._1
       }
     }
     case CUpdate(lhs, rhs) => {
       val (t1, e1) = checkE(lhs)
-      val (t2, e2) = checkE(rhs)(e1, its)
+      val (t2, e2) = checkE(rhs)(e1, rres)
       // :< is a subtyping method we defined on the type trait.
       if (t2 :< t1) (t1, t2, lhs) match {
         // Reassignment of static ints upcasts to bit<32>
@@ -122,7 +142,7 @@ object TypeChecker {
     }
     case CReduce(rop, l, r) => {
       val (t1, e1) = checkE(l)
-      val (ta, e2) = checkE(r)(e1, its)
+      val (ta, e2) = checkE(r)(e1, rres)
       (t1, ta) match {
         case (t1, TArray(t2, dims)) =>
           if (t2 :< t1 == false || dims.length != 1 || dims(0)._1 != dims(0)._2) {
@@ -147,19 +167,19 @@ object TypeChecker {
       // Add binding for iterator in a separate scope.
       val e1 = env.addScope.add(iter -> Info(iter, range.idxType)).addScope
       // Check for body and pop the scope.
-      val (e2, binds) = checkC(par)(e1, range.u :: its).endScope
+      val (e2, binds) = checkC(par)(e1, range.u * rres).endScope
       // Create scope where ids bound in the parallel scope map to fully banked arrays.
       val vecBinds = binds.map({ case (id, inf) =>
         id -> Info(id, TArray(inf.typ, List((range.u, range.u))))
       })
       // Remove the binding of the iterator and add the updated vector bindings.
-      checkC(combine)(e2.endScope._1.addScope ++ vecBinds, its).endScope._1
+      checkC(combine)(e2.endScope._1.addScope ++ vecBinds, rres).endScope._1
     }
     case CExpr(e) => checkE(e)._2
     case CEmpty => env
     case CSeq(c1, c2) => {
       val _ = checkC(c1)
-      val e2 = checkC(c2)(env, its)
+      val e2 = checkC(c2)(env, rres)
       // FIXME(rachit): This should probably be intersection of e1 and e2
       e2
     }
