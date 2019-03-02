cucapra/dahlia

commit 03ff2c79965820cb5cbe05fc46e3467f349f799e

changes to buildbot and Dockerfile

Dockerfile
@@ -1,5 +1,6 @@
-FROM ocaml/opam2:ubuntu-18.04
+FROM hseeberger/scala-sbt
 MAINTAINER Adrian Sampson <asampson@cs.cornell.edu>
+MAINTAINER Rachit Nigam <rnigam@cs.cornell.edu>
 
 # Add Python, pipenv, and node for buildbot.
 RUN sudo apt-get install -y software-properties-common && \
@@ -13,17 +14,10 @@ RUN curl https://bootstrap.pypa.io/get-pip.py | sudo -H python3.7
 ENV PATH ${HOME}/.local/bin:${PATH}
 RUN pip install --user pipenv
 
-# Install some of our OCaml dependencies.
-RUN opam config exec -- opam depext --install -y \
-    dune menhir core ppx_deriving ppx_expect cmdliner
-    
 # Install sshpass
 RUN sudo apt-get install sshpass
 RUN sudo chown opam:opam ~/.ssh
 #RUN sudo chown opam:opam ~/.ssh/*
-
-# Add opam bin directory to our $PATH so we can run seac.
-ENV PATH ${HOME}/.opam/system/bin:${PATH}
 
 # Volume, port, and command for buildbot.
 VOLUME ${HOME}/seashell/buildbot/instance
@@ -39,9 +33,8 @@ ADD --chown=opam . seashell
 WORKDIR seashell
 
 # Build Seashell.
-RUN opam install --deps-only .
-RUN eval `opam config env` ; dune build
-RUN eval `opam config env` ; dune install
+RUN sbt assembly
+ENV PATH ${PWD}
 
 # Avoids a bug in a recent version of pip:
 # https://github.com/pypa/pipenv/issues/2924

buildbot/buildbot/config_default.py
@@ -2,7 +2,7 @@
 UPLOAD_EXTENSIONS = ['zip']
 
 # The executable name for the Seashell compiler (seac).
-SEASHELL_COMPILER = 'seac'
+SEASHELL_COMPILER = 'fuse'
 
 # A prefix command to use *before* the invocations of Xilinx tools.
 HLS_COMMAND_PREFIX = []

commit 522c2eb5e3292618e09a152c889f8dbaa955c1b1

Dockerfile: Fix editing of $PATH
This problem, introduced in 03ff2c79, *replaced* the entire $PATH
environment variable with the cwd, which made all other commands
"unavailable." This now properly just extends the $PATH.

Dockerfile
@@ -31,7 +31,7 @@ WORKDIR /seashell
 
 # Build Seashell.
 RUN sbt assembly
-ENV PATH ${PWD}
+ENV PATH /seashell:${PATH}
 
 # Set up buildbot.
 RUN cd buildbot ; PIPENV_PIPFILE= pipenv install