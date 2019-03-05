fecd763afbe759ec2a6780f8f9ad7f7b6cef33d9
Sachille Atapattu <sa2257@cornell.edu>
2019-03-05 11:54:38 -0500

    Clean-up examples directory (Buildbot synthed examples only)

--- examples/src/README.md
+++
@@ -1,8 +0,0 @@
-# Examples of Seashell
-
-1. vector-scalar add with unroll keyword
-2. vector-scalar add with manual unroll
-3. vector-vector add
-4. matrix add
-5. matrix multiply
-6. gemm ncubed
--- examples/vvadd/vvadd.h
+++ examples/vvadd/vvadd.h
@@ -1,5 +1,5 @@
-#ifndef _MADD_H_
-#define _MADD_H_
+#ifndef _VVADD_H_
+#define _VVADD_H_
 
 #define N 32
 
@@ -8,8 +8,8 @@ static int ELE = N;
 //#pragma SDS data access_pattern(A:RANDOM, B:RANDOM, C:RANDOM)
 //#pragma SDS data copy(A[0:N*N], B[0:N*N], C[0:N*N])
 //#pragma SDS data zero_copy(A[0:N*N], B[0:N*N], C[0:N*N])
-void madd(float *A, float *B, float *C);
+void vvadd(float *A, float *B, float *C);
 
-#endif /* _MADD_H_ */
+#endif /* _VVADD_H_ */
 
 
