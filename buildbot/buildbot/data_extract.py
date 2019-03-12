ed14f70c55dbfe6d91f0d906eb9586ee1be92273
Sachille Atapattu <sa2257@cornell.edu>
2019-03-11 21:38:57 -0400

    Skip extraction if no data file

--- buildbot/buildbot/data_extract.py
+++ buildbot/buildbot/data_extract.py
@@ -27,50 +27,51 @@ def hw_rpt(datawriter, func_hw):
     report = os.path.join(os.getcwd(), '_sds/reports', 'sds_' + func_hw + '.rpt')
     if os.path.isfile(report): # Use with for file open
         data = open(report, "r+")
+    
+        lines = []
+        for line in data:
+            lines.append(line)
+        
+        # extract relevant data # section jump strategy?
+        for num, line in enumerate(lines,1):
+            if '== Performance Estimates' in line:
+                lat = num-1
+            if '== Utilization Estimates' in line:
+                utl = num-1
+        
+        # Read timing details
+        timing  = re.split("\|",''.join(lines[lat+7].split()))
+        latency = re.split("\|",''.join(lines[lat+16].split()))
+        
+        task.log ('Target Clock, Estimated Clock, Min Latency, Max Latency, Pipelining')
+        task.log (timing[2],timing[3],latency[1],latency[2],latency[5])
+        
+        datawriter.writerow(['Target Clock','Estimated Clock','Min latency','Max latency','Pipelining'])
+        datawriter.writerow([timing[2],timing[3],latency[1],latency[2],latency[5]])
+        datawriter.writerow([ ])
+        
+        # Read loop details
+        datawriter.writerow([ ])
+        
+        # Read utilization details
+        for num, line in enumerate(lines[utl:utl+20],1):
+            if 'Name' in line:
+                names = re.split("\|+",''.join(line.split()))[1:-1] # strip away empty elements added
+            if 'Total' in line:
+                total = re.split("\|+",''.join(line.split()))[1:-1]
+            if 'Available' in line:
+                avail = re.split("\|+",''.join(line.split()))[1:-1]
+            if 'Utilization' in line:
+                util  = re.split("\|+",''.join(line.split()))[1:-1]
+        
+        for num in range(len(names)):
+            task.log (names[num],'-',total[num],'of',avail[num],'->',util[num],'%')
+            datawriter.writerow([names[num],total[num],avail[num],util[num]])
+        datawriter.writerow([ ])
+    
     else:
         task.log("sds_" + func_hw + " does not exit!! @ " + report)
-        sys.exit()
-    
-    lines = []
-    for line in data:
-        lines.append(line)
-    
-    # extract relevant data # section jump strategy?
-    for num, line in enumerate(lines,1):
-        if '== Performance Estimates' in line:
-            lat = num-1
-        if '== Utilization Estimates' in line:
-            utl = num-1
-    
-    # Read timing details
-    timing  = re.split("\|",''.join(lines[lat+7].split()))
-    latency = re.split("\|",''.join(lines[lat+16].split()))
-    
-    task.log ('Target Clock, Estimated Clock, Min Latency, Max Latency, Pipelining')
-    task.log (timing[2],timing[3],latency[1],latency[2],latency[5])
-    
-    datawriter.writerow(['Target Clock','Estimated Clock','Min latency','Max latency','Pipelining'])
-    datawriter.writerow([timing[2],timing[3],latency[1],latency[2],latency[5]])
-    datawriter.writerow([ ])
-    
-    # Read loop details
-    datawriter.writerow([ ])
-    
-    # Read utilization details
-    for num, line in enumerate(lines[utl:utl+20],1):
-        if 'Name' in line:
-            names = re.split("\|+",''.join(line.split()))[1:-1] # strip away empty elements added
-        if 'Total' in line:
-            total = re.split("\|+",''.join(line.split()))[1:-1]
-        if 'Available' in line:
-            avail = re.split("\|+",''.join(line.split()))[1:-1]
-        if 'Utilization' in line:
-            util  = re.split("\|+",''.join(line.split()))[1:-1]
-    
-    for num in range(len(names)):
-        task.log (names[num],'-',total[num],'of',avail[num],'->',util[num],'%')
-        datawriter.writerow([names[num],total[num],avail[num],util[num]])
-    datawriter.writerow([ ])
+        #sys.exit()
     
 def dm_rpt(datawriter):    
     ################ Open data motion network file ###############################
@@ -78,42 +79,43 @@ def dm_rpt(datawriter):
     report = os.path.join(os.getcwd(), '_sds/reports', 'data_motion.html')
     if os.path.isfile(report):  # Use with for file open
         data = open(report, "r+")
+    
+        lines = []
+        for line in data:
+            if 'li' in line: # This removes additional lines added to the report for a certain pragma (update this comment)
+                task.log(line)
+            else:
+                lines.append(line)
+        
+        items = 0
+        for num,line in enumerate(lines,1): # section jump strategy?
+            if 'Data Motion Network' in line: # Some data appear in this section
+                data = num-1
+            if 'Accelerator Callsites' in line: # Some data appear in this section
+                acs = num-1
+            if 'PORT' in line: # Assume every interface will have an IP Port called PORT something
+                items += 1
+        
+        items = items//2 # IP Port appears twice in the report for the two tables
+        
+        task.log ('IP port- Connection- Transfer size- Mem layout- Data mover setup time- Transfer time')
+        
+        datawriter.writerow(['IP port','Connection','Transfer size','Mem layout','Data mover setup time','Transfer time'])
+        
+        # loop over port number
+        for item in range(items):
+            ip   = re.findall('(?<=>).*(?=<)',lines[data+13+10*item])
+            conn = re.findall('(?<=>).*(?=<)',lines[data+19+10*item])
+            tsiz = re.findall('(?<=>).*(?=<)',lines[acs+14+8*item])
+            meml = re.findall('(?<=>).*(?=<)',lines[acs+15+8*item])
+            dmst = re.findall('(?<=>).*(?=<)',lines[acs+16+8*item])
+            trft = re.findall('(?<=>).*(?=<)',lines[acs+17+8*item])
+            task.log(ip[0],conn[0],tsiz[0],meml[0],dmst[0],trft[0])
+            datawriter.writerow([ip[0],conn[0],tsiz[0],meml[0],dmst[0],trft[0]])
+        
     else:
         task.log("data_motion does not exit!! @ " + report)
-        sys.exit()
-    
-    lines = []
-    for line in data:
-        if 'li' in line: # This removes additional lines added to the report for a certain pragma (update this comment)
-            task.log(line)
-        else:
-            lines.append(line)
-    
-    items = 0
-    for num,line in enumerate(lines,1): # section jump strategy?
-        if 'Data Motion Network' in line: # Some data appear in this section
-            data = num-1
-        if 'Accelerator Callsites' in line: # Some data appear in this section
-            acs = num-1
-        if 'PORT' in line: # Assume every interface will have an IP Port called PORT something
-            items += 1
-    
-    items = items//2 # IP Port appears twice in the report for the two tables
-    
-    task.log ('IP port- Connection- Transfer size- Mem layout- Data mover setup time- Transfer time')
-    
-    datawriter.writerow(['IP port','Connection','Transfer size','Mem layout','Data mover setup time','Transfer time'])
-    
-    # loop over port number
-    for item in range(items):
-        ip   = re.findall('(?<=>).*(?=<)',lines[data+13+10*item])
-        conn = re.findall('(?<=>).*(?=<)',lines[data+19+10*item])
-        tsiz = re.findall('(?<=>).*(?=<)',lines[acs+14+8*item])
-        meml = re.findall('(?<=>).*(?=<)',lines[acs+15+8*item])
-        dmst = re.findall('(?<=>).*(?=<)',lines[acs+16+8*item])
-        trft = re.findall('(?<=>).*(?=<)',lines[acs+17+8*item])
-        task.log(ip[0],conn[0],tsiz[0],meml[0],dmst[0],trft[0])
-        datawriter.writerow([ip[0],conn[0],tsiz[0],meml[0],dmst[0],trft[0]])
+        #sys.exit()
 
 #def runtime_data():
     
