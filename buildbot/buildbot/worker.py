cucapra/dahlia

commit 9bf5aaa7c848e687a721163f5d5f0c2ddd531c85

Try hard to avoid crash on missing options

buildbot/buildbot/worker.py
@@ -158,7 +158,6 @@ def _task_config(task, config):
         config['DEFAULT_PLATFORM']
 
 
-
 class WorkThread(threading.Thread):
     """A base class for all our worker threads, which run indefinitely
     to process tasks in an appropriate state.
@@ -218,10 +217,14 @@ def stage_make(db, config):
     with work(db, state.MAKE, state.MAKE_PROGRESS, state.HLS_FINISH) as task:
         _task_config(task, config)
 
-        if task['platform']:
-            task.log('WARNING: make stage is ignoring platform={}'.format(task['platform']))
-        if task['sdsflags']:
-            task.log('WARNING: make stage is ignoring sdsflags={}'.format(task['sdsflags']))
+        if task.get('platform'):
+            task.log('WARNING: make stage ignoring platform={}'.format(
+                task['platform']
+            ))
+        if task.get('sdsflags'):
+            task.log('WARNING: make stage is ignoring sdsflags={}'.format(
+                task['sdsflags']
+            ))
 
         task.run(
             prefix + [

commit 1da13f78a7a1cdf850d65a7b0a078e1e1496457d

Fix #172
This was my fault, introduced in
9bf5aaa7c848e687a721163f5d5f0c2ddd531c85! I forgot tasks weren't proper
dictionaries.

buildbot/buildbot/worker.py
@@ -217,11 +217,11 @@ def stage_make(db, config):
     with work(db, state.MAKE, state.MAKE_PROGRESS, state.HLS_FINISH) as task:
         _task_config(task, config)
 
-        if task.get('platform'):
+        if task['platform']:
             task.log('WARNING: make stage ignoring platform={}'.format(
                 task['platform']
             ))
-        if task.get('sdsflags'):
+        if task['sdsflags']:
             task.log('WARNING: make stage is ignoring sdsflags={}'.format(
                 task['sdsflags']
             ))