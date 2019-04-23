3da73c0c4ac2efa8668c90cd96f13697d5ab8f24
Rachit Nigam <rachit.nigam12@gmail.com>
2019-04-23 15:34:09 -0400

    fix split example in racket

--- tools/array-access-visualizer/split-example.rkt
+++ tools/array-access-visualizer/split-example.rkt
@@ -6,22 +6,22 @@
 ;; B_v banks and the underlying array has B_a banks, returns
 ;; the index access on the corresponding underlying array.
 (define (split i j B_a B_v)
-  (+ (* i (/ B_a B_v)) (modulo j B_v) (* B_a (floor (/ j B_v))) ))
+  (+ (* i B_v) (modulo j B_v) (* B_a (floor (/ j B_v)))))
 
 (define-values (A V)
-  (let ([B_a 4]
-        [B_v 2])
-    (let/matrix [ (A[1][8 #:bank B_a] )
-                  (V[2 #:bank 2][4 #:bank 2]) ]
-                (for ([i (in-range 2)]
-                      [j (in-range 4)])
+  (let* ([B-a 10]
+         [B-v ]
+         [L-a (* B-a 2)])
+    (let/matrix [ (A[1][L-a #:bank B-a] )
+                  (V[B-v #:bank B-v][(/ L-a B-v) #:bank (/ B-a B-v)]) ]
+                (for ([i (in-range B-v)]
+                      [j (in-range (/ L-a B-v))])
                   (V[i][j])
-                  (A[0][(split i j B_a B_v)])))))
+                  (A[0][(split i j B-a (/ B-a B-v))])))))
 
 (require pict)
 (scale
  (for/fold ([acc (blank 0)])
            ([e1 V]
             [e2 A])
-   (hc-append 10 acc (vc-append 4 e1 e2)))
- 1.7)
+   (hc-append 10 acc (vc-append 4 e1 e2))) 0.8)
