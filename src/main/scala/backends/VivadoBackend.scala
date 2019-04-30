681d84f9981601c7eecc1312485a5d98db8c40a0
Rachit Nigam <rachit.nigam12@gmail.com>
2019-04-30 17:38:47 -0400

    fixes #187

--- src/main/scala/backends/VivadoBackend.scala
+++ src/main/scala/backends/VivadoBackend.scala
@@ -39,7 +39,7 @@ private class VivadoBackend extends CppLike {
     emitType(ta.typ) <+> id <> generateDims(ta.dims)
 
   def generateDims(dims: List[(Int, Int)]): Doc =
-    brackets(value(dims.foldLeft(1)({ case (acc, (l, _)) => acc * l})))
+    ssep(dims.map(d => brackets(value(d._1))), emptyDoc)
 
   def emitType(typ: Type) = typ match {
     case _:TVoid => "void"
