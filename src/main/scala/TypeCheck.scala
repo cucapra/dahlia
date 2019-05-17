746226491dd5d7fd64a10e7b241d22e30df81f0b
Rachit Nigam <rachit.nigam12@gmail.com>
2019-05-17 17:48:15 -0400

    words

--- src/main/scala/TypeCheck.scala
+++ src/main/scala/TypeCheck.scala
@@ -12,7 +12,7 @@ import Logger.PositionalLoggable
 /**
  * Type checker implementation for Fuse. Apart from normal typechecking, such as
  * ensuring condition in `if` is a boolean, it does following.
- * - Linear properties of banks in memories.
+ * - Affine properties of banks in memories.
  * - Checking combine blocks correctly use bound variables.
  * - Generate subtype joins for binary operators.
  * - Checks read/write capabilites for array accesses
@@ -20,25 +20,41 @@ import Logger.PositionalLoggable
  * It also MUTATES and ANNOTATES the input AST:
  * - CLet with an explicit type get the correct type
  * - Binders for id update the `typ` var defined on the Id class.
- */
-
-/**
- * Type checking array accesses is the most important and complex step.
- * Here is the pseudocode for the algorithm:
  *
- *  function consume_banks(expr, env):
- *    consume banks and return product of static parts of index types of accessors.
+ * Type checking array accesses enforces the various affine restrictions. A
+ * lot of machinery works together to implement gadget checking.
  *
- *  // TODO(rachit): Update this algorithm
- *  function check_eaa(env, cap_env, req_resources, required_cap, expr):
- *    if cap_env(expr) == R and required_cap == W: FAIL
- *    else if cap_env(expr) == R: SUCCESS
- *    else if cap_env(expr) == W: FAIL
- *    else:
- *      n = consume_banks(expr, env)
- *      if required_cap == W and req_resources != n: FAIL
- *      cap_env(expr).bind(req_resources)
- *      SUCCESS
+ * We make a distinction between physical memories ("memories") and
+ * the hardware that accesses them ("gadgets"). In short, the type checker
+ * creates affine resources corresponding to memories and makes sure that
+ * different gadgets with the same underlying memory don't try to consume
+ * more resources than a memory provides.
+ *
+ * '''Memory Instantiation'''
+ *
+ * The type checker starts tracking memory resource after they are created.
+ * There are two ways of creating memories:
+ *
+ * 1. `decl`s with array types.
+ * 2. `let` bindings with array types.
+ *
+ * When the type checker encounters these, it adds two things to the context:
+ *
+ * 1. The physical affine resources implied by the type. For example, the
+ *    type bit<32>[10 bank 5][4 bank 4] implies that the memory has 5 affine
+ *    resources in the first dimension and 4 in the second.
+ * 2. A default gadget for this memory ([[Gadgets.BaseGadget]]). All possible
+ *    compositions of gadgets have a [[Gadgets.BaseGadget]] at their root.
+ *
+ * '''Gadget Creation'''
+ *
+ * Gadgets are created in two places:
+ *
+ * 1. Default ([[Gadgets.BaseGadget]]) when a memory definition is reached.
+ * 2. View ([[Gadgets.ViewGadget]]) when a view is created. A view gadget is
+ *    built on top of another gadget itself which might come from a memory
+ *    or another gadget itself. This creates a hierarchy of gadgets.
+ *
  */
 object TypeChecker {
 
