47dc8f7ad9090a0dc58505546b3057f52384c0d8
Sachille Atapattu <sa2257@cornell.edu>
2019-06-03 04:05:51 -0400

    move file copy to stage_hls as makefiles do file copy

--- buildbot/buildbot/worker.py
+++ buildbot/buildbot/worker.py
@@ -344,6 +344,14 @@ def stage_hls(db, config):
             cwd=CODE_DIR,
         )
 
+        # Copy datafiles to executable dir.
+        data_files = [os.path.join(task.code_dir, f) for f in os.listdir(task.code_dir) if f.endswith('.data')]
+        dest = os.path.join(task.code_dir, 'sd_card')
+        if data_files:
+            task.run(
+                ['cp'] + data_files + [dest]
+            )
+        
 
 def stage_fpga_execute(db, config):
     """Work stage: upload bitstream to the FPGA controller, run the
@@ -361,14 +369,6 @@ def stage_fpga_execute(db, config):
             task.log('skipping FPGA execution stage')
             return
 
-        # Copy datafiles to executable dir.
-        data_files = [os.path.join(task.code_dir, f) for f in os.listdir(task.code_dir) if f.endswith('.data') if f is not None]
-        dest = os.path.join(task.code_dir, 'sd_card')
-        if data_files:
-            task.run(
-                ['cp'] + data_files + [dest]
-            )
-        
         # Copy the compiled code (CPU binary + FPGA bitstream) to the
         # Zynq board.
         bin_dir = os.path.join(task.code_dir, 'sd_card')
