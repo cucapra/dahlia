85165deef689f76156a33e82a4130b29c8918a07
Samuel Thomas <sgpthomas@gmail.com>
2019-06-24 14:47:37 -0400

    Removed duplicate configuration by leaving backend as string a little longer

--- src/main/scala/common/Configuration.scala
+++ src/main/scala/common/Configuration.scala
@@ -4,13 +4,20 @@ import java.io.File
 
 object Configuration {
 
+  sealed trait Mode
+  final case object Compile extends Mode
+  final case object Run extends Mode
+
+  val emptyConf = Config(null)
+
   case class Config(
     srcFile: File,                        // Required: Name of the source file
     kernelName: String = "kernel",        // Name of the kernel to emit
     output: Option[String] = None,        // Name of output file.
+    backend: String = "vivado",           // Backend used for code generation
+    mode: Mode = Compile,                 // Compilation mode
     compilerOpts: List[String] = List(),  // Extra options to the generateExec Compiler
     header: Boolean = false,              // Generate a header
     logLevel: scribe.Level = scribe.Level.Info
   )
-
 }
