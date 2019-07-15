316c6c6ece9b689f81e9b5ad6cb18021bc53e47e
KarlMobed <kem275@cornell.edu>
2019-07-15 14:09:42 -0400

    And so it goes

--- src/main/scala/backends/IntelBackend.scala
+++ src/main/scala/backends/IntelBackend.scala
@@ -11,13 +11,14 @@ private class IntelBackend extends CppLike {
 
   def unroll(n: Int): Doc = n match {
            case 1 => emptyDoc
-           case n => value(s"__attribute__((opencl_unroll_hint($n)))") <@> line
+           case n => value(s"#pragma unroll $n\n")
      }
 
 
   def bank(id: Id, banks: List[Int]): String = banks.zipWithIndex.foldLeft(""){
     case (acc, (bank, dim)) =>
       if (bank != 1) {
+        //s"${acc}"__attribute__((xcl_array_partition(block, <factor>, 1)))
         s"${acc}\n#pragma HLS ARRAY_PARTITION variable=$id factor=$bank dim=${dim + 1}"
       } else {
         acc
@@ -30,8 +31,8 @@ private class IntelBackend extends CppLike {
     .map(s => value(s))
 
   def emitFor(cmd: CFor): Doc =
+    unroll(cmd.range.u) <+>
     "for" <> emitRange(cmd.range) <+> scope {
-      unroll(cmd.range.u) <>
       cmd.par <>
       (if (cmd.combine != CEmpty) line <> text("// combiner:") <@> cmd.combine
        else emptyDoc)
@@ -42,7 +43,7 @@ private class IntelBackend extends CppLike {
   }
 
   def emitArrayDecl(ta: TArray, id: Id) =
-    emitType(ta.typ) <+> "buffer" <+> id <> generateDims(ta.dims)
+    emitType(ta.typ) <+> id <> generateDims(ta.dims)
 
   def generateDims(dims: List[(Int, Int)]): Doc =
     ssep(dims.map(d => brackets(value(d._1))), emptyDoc)
@@ -63,11 +64,10 @@ private class IntelBackend extends CppLike {
     val layout =
       vsep(p.includes.map(emitInclude)) <@>
       vsep(p.defs.map(emitDef)) <@>
-      emitFunc(FuncDef(Id(c.kernelName), p.decls, Some(p.cmd)))
+      "__kernel " <> emitFunc(FuncDef(Id(c.kernelName), p.decls, Some(p.cmd)))
 
     super.pretty(layout).layout
   }
-
 }
 
 private class IntelBackendHeader extends IntelBackend {
@@ -89,8 +89,8 @@ private class IntelBackendHeader extends IntelBackend {
 
 case object IntelBackend extends Backend {
   def emitProg(p: Prog, c: Config) = c.header match {
-    case true => (new VivadoBackendHeader()).emitProg(p, c)
-    case false => (new VivadoBackend()).emitProg(p, c)
+    case true => (new IntelBackendHeader()).emitProg(p, c)
+    case false => (new IntelBackend()).emitProg(p, c)
   }
   val canGenerateHeader = true
 }
