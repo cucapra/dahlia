44a42ca1030d949f3f13af03f405adec833aa349
Rachit Nigam <rachit.nigam12@gmail.com>
2019-07-28 12:23:23 -0700

    split example script for generating images in the paper

--- tools/array-access-visualizer/split-example.rkt
+++ tools/array-access-visualizer/split-example.rkt
@@ -1,6 +1,15 @@
 #lang racket/base
 
-(require "let-matrix.rkt")
+(require racket/list)
+
+(require "let-matrix.rkt"
+         "image-gen-helpers.rkt")
+(require pict)
+
+(define (merge-imgs A V)
+  (zip-with
+      (lambda (e1 e2) (vc-append 4 e1 e2))
+      A V))
 
 ;; For a given split view access v[i][j] where view has
 ;; B_v banks and the underlying array has B_a banks, returns
@@ -8,20 +17,52 @@
 (define (split i j B_a B_v)
   (+ (* i B_v) (modulo j B_v) (* B_a (floor (/ j B_v)))))
 
+; Show what splitting looks like
+(define-values (A-simpl V-simpl)
+  (let* ([B-a 4]
+         [B-v 2]
+         [L-a (* B-a 2)])
+    (let/matrix [ (A[1][L-a #:bank B-a] )
+                  (V[B-v #:bank B-v][(/ L-a B-v) #:bank (/ B-a B-v)]) ])))
+
+(save-pict
+  (scale (smart-layout (merge-imgs A-simpl V-simpl)) 2) "split.png" 'png)
+
+
+; Show what splitting and parallelizing the inner loop looks like
 (define-values (A V)
-  (let* ([B-a 6]
-         [B-v 3]
+  (let* ([B-a 4]
+         [B-v 2]
          [L-a (* B-a 2)])
     (let/matrix [ (A[1][L-a #:bank B-a] )
                   (V[B-v #:bank B-v][(/ L-a B-v) #:bank (/ B-a B-v)]) ]
-                (for ([i (in-range B-v)]
-                      [j (in-range (/ L-a B-v))])
-                  (V[i][j])
-                  (A[0][(split i j B-a (/ B-a B-v))])))))
+      (for ([j (in-range (/ L-a B-v))])
+        (for* ([i (in-range B-v)])
+          (V[i][j])
+          (A[0][(split i j B-a (/ B-a B-v))]))))))
 
-(require pict)
-(scale
- (for/fold ([acc (blank 0)])
-           ([e1 V]
-            [e2 A])
-   (hc-append 10 acc (vc-append 4 e1 e2))) 0.8)
+; Drop last element of the list
+(define img-lst
+  (let ([tmp-lst (merge-imgs A V)])
+    (take tmp-lst (sub1 (length tmp-lst)))))
+
+(save-pict (scale (smart-layout img-lst) 2) "split-outer.png" 'png)
+
+; Show what splitting and parallelizing the outer loop looks like
+#|(define-values (A-outr V-outr)
+  (let* ([B-a 4]
+         [B-v 2]
+         [L-a (* B-a 2)])
+    (let/matrix [ (A[1][L-a #:bank B-a] )
+                  (V[B-v #:bank B-v][(/ L-a B-v) #:bank (/ B-a B-v)]) ]
+      (for ([i (in-range B-v)])
+        (for* ([j (in-range (/ L-a B-v))])
+          (V[i][j])
+          (A[0][(split i j B-a (/ B-a B-v))]))))))
+
+; Drop last element of the list
+(define img-lst-outr
+  (let ([tmp-lst (merge-imgs A-outr V-outr)])
+    (take tmp-lst (sub1 (length tmp-lst)))))
+
+(save-pict (scale (smart-layout img-lst-outr) 2) "split-outer.png" 'png)|#
