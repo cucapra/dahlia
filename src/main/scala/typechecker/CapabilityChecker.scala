1682b00a367dab2b7fa537a87c5f5e6c6b92d7dd
Rachit Nigam <rachit.nigam12@gmail.com>
2019-10-04 15:29:38 -0400

    write and read capabilities are always acquired if skip annotation is not found

--- src/main/scala/typechecker/CapabilityChecker.scala
+++ src/main/scala/typechecker/CapabilityChecker.scala
@@ -19,13 +19,17 @@ object CapabilityChecker {
 
     val emptyEnv = CapabilityEnv.emptyEnv
 
-
+    /**
+     * Check an array read.
+     * - If there is already a Read capability for this expression, ignore.
+     * - If there are no capabilities or a write capability, add a consume
+     *   annotation.
+     */
     override def myCheckE: PF[(Expr, Env), Env] = {
       case (acc@EArrAccess(_, idxs), env) => {
         val (nEnv, consumableAnn, cap) = env.get(acc) match {
-          case Some(Write) => throw InvalidCap(acc, Read, Write)
           case Some(Read) => (env, SkipConsume, Read)
-          case None => (checkESeq(idxs)(env), ShouldConsume, Read)
+          case Some(Write) | None => (checkESeq(idxs)(env), ShouldConsume, Read)
         }
         acc.consumable = Some(consumableAnn)
         nEnv.add(acc, cap)
@@ -37,6 +41,9 @@ object CapabilityChecker {
     }
 
     /**
+     * Check an array write. If there is already a write capability, error.
+     * Otherwise try to acquire a write capability.
+     *
      * This doesn't need to be partial function since it deals with all
      * cases in checkLVal.
      */
@@ -44,8 +51,7 @@ object CapabilityChecker {
       case acc@EArrAccess(_, idxs) => {
         val (nEnv, consumableAnn, cap) = env.get(e) match {
           case Some(Write) => throw AlreadyWrite(e)
-          case Some(Read) => throw InvalidCap(e, Write, Read)
-          case None => (checkESeq(idxs), ShouldConsume, Write)
+          case Some(Read) | None => (checkESeq(idxs), ShouldConsume, Write)
         }
 
         acc.consumable = Some(consumableAnn)
