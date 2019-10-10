6aa0fce02286e1970407fb8a9319d5ac02bcf01f
Zhijing <173666635@qq.com>
2019-10-09 22:36:13 -0400

    withscope need more work

--- src/main/scala/common/ScopeMapExt.scala
+++ src/main/scala/common/ScopeMapExt.scala
@@ -3,7 +3,7 @@ package fuselang.common
 import CompilerError._
 import fuselang.Utils._
 
-object ScopedMapExt {
+object ScopeMapExt {
 
   /**
    * A map that undestands scopes. A ScopedMap is a chain of maps from
@@ -18,43 +18,45 @@ object ScopedMapExt {
         .map(map => s"<${map.map({case (k, v) => s"$k -> $v"}).mkString(", ")}>")
         .mkString(" => ")
 
-    def get(key: K): Option[V] =
-      mapList.head.get(key)
+    def isEmpty: Boolean = mapList.isEmpty
+    def get(key: K): Option[V] = mapList.head.get(key)
       //mapList.find(map => map.get(key).isDefined).map(c => c(key))
 
     /**
      * Add key -> value binding to the topmost scope.
-     * @returns None if the value is already bound in the scope chain, otherwise
-     *          a new [[ScopedMap]] with the binding in the top most scope.
      */
-    def add(key: K, value: V): Option[ScopedMapExt[K, V]] = get(key) match {
-      case Some(_) => None
-      case None => Some(this.copy(mapList = mapList.head + (key -> value) :: mapList.tail))
+    def add(key: K, value: V): ScopedMapExt[K, V] = {
+      this.copy(mapList = mapList.head + (key -> value) :: mapList.tail)
     }
+    /*
+    get(key) match {
+      case Some(_) => this
+      case None => this.copy(mapList = mapList.head + (key -> value) :: mapList.tail)
+    }*/
 
     /**
      * Update the binding for [[key]] to [[value]]. The update method walks
      * the scope chain to find where the implementation is bound and update it.
      * @returns a new [[ScopedMap]] with the key bound to value
      * @throw [[Errors.Unbound]] If the key is not found.
-     */
+    
     def update(key: K, value: V): ScopedMapExt[K, V] = {
       val scope = mapList.indexWhere(m => m.get(key).isDefined)
       assertOrThrow(scope > -1, Impossible(s"$key was not found."))
       val newMapList = mapList.updated(scope, mapList(scope) + (key -> value))
       this.copy(mapList = newMapList)
-    }
-
+    }*/
+    
     /** Methods to manage scopes. */
     def addScope: ScopedMapExt[K, V] = ScopedMapExt(Map[K, V]() :: mapList)
 
     def endScope: Option[(Map[K, V], ScopedMapExt[K, V])] = mapList match {
       case Nil => None
-      case hd :: tl => Some((hd, this.copy(mapList = tl)))
+      case hd :: tl => Some( (hd, this.copy(mapList = tl)) )
     }
 
-    /** Return the set of all keys. */
-    def keys = mapList.flatMap(m => m.keys).toSet
+    /** Return the set of keys at head. */
+    def keys = mapList.head.keySet
 
     // Convinience methods
     def apply(k: K) = get(k).getOrThrow(Impossible(s"$k was not found in $this."))
--- src/main/scala/passes/LoopCheck.scala
+++ src/main/scala/passes/LoopCheck.scala
@@ -2,14 +2,15 @@ package fuselang.passes
 
 import scala.{PartialFunction => PF}
 
-import fuselang.Utils.RichOption
+//import fuselang.Utils.RichOption
 
 import fuselang.common._
 import ScopeMap._
+import ScopeMapExt._
 import Syntax._
-import Errors._
+//import Errors._
 import CompilerError._
-import Logger._
+//import Logger._
 import Checker._
 import EnvHelpers._
 
@@ -20,30 +21,113 @@ object LoopChecker {
   // Bounds checker environment doesn't need to track any information. Empty
   // environment that just runs the commands.
   private case class LEnv(
-    UseMap: ScopedMap[Id, Boolean] = ScopedMap(),
-    NameMap: Map[Id,Id] = Map())(implicit val res: Int) 
+    StateMap: ScopedMapExt[Id, Int] = ScopedMapExt(),
+    NameMap: ScopedMap[Id,Id] = ScopedMap())(implicit val res: Int) 
     extends ScopeManager[LEnv] {
     
-    // Helper functions for ScopeManager/
-    def addScope(resources: Int) = {
-      LEnv(UseMap.addScope, NameMap)(res * resources)
+    //states 
+    val USE = 0
+    val DEF = 1
+    val DK = 2
+    
+    // Whether we need to check loop dependency depends on 
+    // 1. if there is a loop
+    // 2. if the loop needs unrolling
+    def needCheck: Boolean = res > 1
+    
+    // Helper functions for NameMap
+    def addName(vid:Id, tid:Id):LEnv = NameMap.add(vid,tid) match{
+      case None => {Impossible("NameMap has this view id before, redefinition");this}
+      case Some(m) => LEnv(StateMap,m)
     }
-    def withScope(resources: Int)(inScope: LEnv => LEnv): LEnv = {
-      inScope(this.addScope(resources)) match {
-        case env:LEnv => env.endScope(resources)
+    def getName(aid:Id):Id = NameMap.get(aid).getOrElse(aid)
+    
+    // Helper functions for StateMap
+    def atDef(id:Id): LEnv = StateMap.get(id) match{
+      case None | Some(DK) => LEnv(StateMap+(id->DEF), NameMap)//DK update to DEF
+      case Some(DEF) => this 
+      case Some(USE) => {Impossible("Loop cannot be unrolled");this} //impossible->error 
+      case _ => throw Impossible("No such state")
+    }
+    def atDk(id:Id):LEnv = StateMap.get(id) match{
+      case None | Some(DEF) => LEnv(StateMap+(id->DK), NameMap)
+      case Some(DK) => this
+      case Some(USE) => {Impossible("Loop cannot be unrolled");this} //impossible->error 
+      case _ => throw Impossible("No such state")
+    }
+    def atUse(id:Id):LEnv = StateMap.get(id) match {
+      case None => LEnv(StateMap+(id->USE), NameMap) 
+      case Some(DK) => {Impossible("Loop cannot be unrolled") ;this}
+      case Some(DEF)| Some(USE) => this //Use/Def -> Use don't update
+      case _ => throw Impossible("No such state")
+    }
+    //check and update the state table
+    def updateState(id:Id, state: Int):LEnv = state match {
+      case DK => atDk(id)
+      case DEF => atDef(id)
+      case USE => atUse(id)
+      case _ => throw Impossible("No such state")
+    }
+    // Helper functions for ScopeManager
+    def withScope(inScope: LEnv => LEnv): LEnv = this
+    def withScope(resources: Int, loop: Boolean)(inScope: LEnv => LEnv): LEnv = {
+      inScope(this.addScope(resources, loop)) match {
+        case env:LEnv => env.endScope(resources, loop)
       }
     }
-    def endScope(resources: Int) = {
-      val scopes = for {
-        (_, uMap) <- UseMap.endScope
-      } yield LEnv(uMap,NameMap)(res / resources)
-
-      scopes.getOrThrow(Impossible("Removed topmost scope"))
+    def addScope(resources: Int, loop:Boolean) = {
+      if (res * resources > 1 && loop == true)
+        LEnv(StateMap.addScope, NameMap.addScope)(res * resources)
+      else 
+        LEnv(StateMap, NameMap.addScope)(res * resources)
     }
-    // withScope need to know unroll factor. 
-    // Neither default functions are needed.
-    def withScope(inScope: LEnv => LEnv): LEnv = inScope(this)
-    def merge(that: LEnv): LEnv = this
+    def endScope(resources: Int, loop:Boolean = false) = {
+      val nmap = NameMap.endScope.get._2
+      if (res > 1 && loop == true){
+        val keys = StateMap.keys
+        val (innermap, outermap) = StateMap.endScope.get
+        var outerenv = LEnv(outermap, nmap)(res/resources)
+        for (k <- keys) outerenv = outerenv.updateState(k, innermap(k))      
+        outerenv
+      }
+      else{
+        this.copy(NameMap = nmap)
+      }
+    }
+    // If statement
+    def merge(that: LEnv): LEnv = {
+      val m1 = this.StateMap
+      val m2 = that.StateMap
+      var env = LEnv(m1, NameMap)
+      for (k <- m1.keys){
+        val v = m1.get(k) 
+        env = m2.get(k) match{
+          case Some(DEF) => v match {
+            case Some(DEF) => env
+            case Some(USE) => throw Impossible("Conflict in stateMap, cannot merge to one environment")
+            case Some(DK) | None => env.copy(StateMap = env.StateMap + (k->DK) )
+            case _ => throw Impossible("No such state")
+          }
+          case Some(DK) => v match {
+            case Some(USE) => throw Impossible("Conflict in stateMap, cannot merge to one environment")
+            case  Some(DK) | Some(DEF) | None => env.copy(StateMap = env.StateMap + (k->DK) )
+            case _ => throw Impossible("No such state")
+          }
+          case Some(USE) => v match {
+            case None | Some(USE) => env.copy(StateMap = env.StateMap + (k->USE) )
+            case Some(DK) | Some(DEF) => throw Impossible("Conflict in stateMap, cannot merge to one environment")
+            case _ => throw Impossible("No such state")
+          }
+          case None => v match {
+            case Some(DK) | Some(DEF) => env.copy(StateMap = env.StateMap + (k->USE) )
+            case Some(USE) => env
+            case _ => throw Impossible("No such state")
+          }
+          case _ => throw Impossible("No such state")
+        }
+      }
+      env
+    }
   }
 
   private final case object LCheck extends PartialChecker {
@@ -52,80 +136,61 @@ object LoopChecker {
 
     val emptyEnv = LEnv()(1)
 
-    /**
-     * Given a view with a known prefix length, check if it **might** cause an
-     * out of bound access when accessed.
-     */
-    private def checkView(arrLen: Int, viewId: Id, view: View) = {
-      if (view.prefix.isDefined) {
-        val View(suf, Some(pre), _) = view
-
-        val (sufExpr, fac) = suf match {
-          case Aligned(fac, e) => (e, fac)
-          case Rotation(e) => (e, 1)
-        }
-
-        val maxVal: Int =
-          sufExpr.typ
-            .getOrThrow(Impossible(s"$sufExpr is missing type"))
-            .matchOrError(viewId.pos, "view", "Integer Type"){
-              case idx:TIndex => fac * idx.maxVal
-              case TStaticInt(v) => fac * v
-              case idx:TSizedInt =>
-                scribe.warn(
-                  (s"$idx is used to create view $viewId. This could be unsafe.", idx));
-                1
-            }
-
-        if (maxVal + pre > arrLen) {
-          throw IndexOutOfBounds(viewId, arrLen, maxVal + pre, viewId.pos)
-        }
+    def myCheckLVal(e: Expr, env: Env): Env = { e match 
+      {
+        case EVar(id) => env.atDef(env.getName(id))
+        case EArrAccess(id, _) => env.atDef(env.getName(id))
+        //case ERecAccess(rec, _) => env.atDef(rec)//TODO: fix it
+        case _ => {Impossible("Cannot be lhs value"); env}//throw an error here
       }
     }
-
-
+    
     override def myCheckE: PF[(Expr, Env), Env] = {
-      case (EArrAccess(id, idxs), e) => {
-        id.typ
-          .getOrThrow(Impossible(s"$id missing type in $e"))
-          .matchOrError(id.pos, "array access", s"array type"){
-            case TArray(_, dims) =>
-              idxs
-                .map(idx => idx -> idx.typ)
-                .zip(dims)
-                .foreach({
-                  case ((idx, t), (size, _)) => t.foreach({
-                    case idxt@TSizedInt(n, _) =>
-                      if (math.pow(2, n) >= size) {
-                        scribe.warn(
-                          (s"$idxt is used for an array access. " +
-                            "This might be out of bounds at runtime.", idx))
-                      }
-                    case TStaticInt(v) =>
-                      if (v >= size)
-                        throw IndexOutOfBounds(id, size, v, idx.pos)
-                    case t@TIndex(_, _) =>
-                      if (t.maxVal >= size)
-                        throw IndexOutOfBounds(id, size, t.maxVal, idx.pos)
-                    case t => throw UnexpectedType(id.pos, "array access", s"[$t]", t)
-                  })
-                })
-          }
-        e
-      }
+    // by default, this is rval
+      case (EVar(id), e) => e.atUse(e.getName(id))
+      case (EArrAccess(id, _), e) => e.atUse(e.getName(id))
     }
-
-
+    
     override def myCheckC: PF[(Command, Env), Env] = {
-      case (c@CView(viewId, arrId, views), e) => {
-        val typ = arrId.typ.getOrThrow(Impossible(s"$arrId is missing type in $c"))
-        typ.matchOrError(c.pos, "view", "array type"){ case TArray(_, dims) =>
-          views.zip(dims).foreach({ case (view, (len, _)) =>
-            checkView(len, viewId, view)
-          })
+      case (CUpdate(lhs, rhs), e) => checkE(rhs)(myCheckLVal(lhs,e))
+      case (CReduce(_, lhs, rhs), e) => checkE(rhs)(myCheckLVal(lhs,e))
+      case (_@CView(viewId, arrId, _), e) => {
+        val id = e.getName(arrId)
+        e.addName(viewId,id)
+      }        
+      case (_@CSplit(viewId, arrId, _), e) => {
+        val id = e.getName(arrId)
+        e.addName(viewId,id)
+      }      
+      case (CFor(range, _, par, _), e) => {
+        e.withScope(range.u, true){ e1=>
+          checkC(par)(e1)
         }
-        e
+      } 
+      case (CWhile(cond, _, body), e) => {
+        val e1 = checkE(cond)(e)
+        e1.withScope(1, false)(env => checkC(body)(env))
       }
+      /*
+        cmd match{
+          case CUpdate(lhs, rhs) => checkE(rhs)(myCheckLVal(lhs,e))
+          case CReduce(_, lhs, rhs) => checkE(rhs)(myCheckLVal(lhs,e))
+          case _@CView(viewId, arrId, _) => {
+            val id = e.getn(arrId)
+            e.addn(viewId,id)
+          }      
+          case _@CSplit(viewId, arrId, _) => {
+            val id = e.getn(arrId)
+            e.addn(viewId,id)
+          }      
+          case CFor(range, _, par, _) => {
+            e.withScope(range.u){ e1=>
+              checkC(par)(e1)
+            }
+          }  
+        }
+      else
+        e*/
     }
   }
 }
