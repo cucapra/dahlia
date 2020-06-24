fcb83cdf5ffd3724837027702dc70389b6870218
Rachit Nigam <rachit.nigam12@gmail.com>
2020-06-24 15:19:06 -0400

    compiler indent

--- src/main/scala/Compiler.scala
+++ src/main/scala/Compiler.scala
@@ -21,24 +21,26 @@ object Compiler {
 
   def toBackend(str: BackendOption): fuselang.backend.Backend = str match {
     case Vivado => backend.VivadoBackend
-    case Cpp    => backend.CppRunnable
-    case Futil  => backend.futil.FutilBackend
+    case Cpp => backend.CppRunnable
+    case Futil => backend.futil.FutilBackend
   }
 
   def checkStringWithError(prog: String, c: Config = emptyConf) = {
     val ast = FuseParser.parse(prog)
     passes.WellFormedChecker.check(ast)
     typechecker.TypeChecker.typeCheck(ast); showDebug(ast, "Type Checking", c)
-    passes.BoundsChecker.check(ast);      // Doesn't modify the AST.
-    passes.LoopChecker.check(ast);        // Doesn't modify the AST.
-    passes.DependentLoops.check(ast);     // Doesn't modify the AST.
-    typechecker.CapabilityChecker.check(ast); showDebug(ast, "Capability Checking", c)
+    passes.BoundsChecker.check(ast); // Doesn't modify the AST.
+    passes.LoopChecker.check(ast); // Doesn't modify the AST.
+    passes.DependentLoops.check(ast); // Doesn't modify the AST.
+    typechecker.CapabilityChecker.check(ast);
+    showDebug(ast, "Capability Checking", c)
     typechecker.AffineChecker.check(ast); // Doesn't modify the AST
     ast
   }
 
   def codegen(ast: Prog, c: Config = emptyConf) = {
-    val rast = passes.RewriteView.rewriteProg(ast); showDebug(rast, "Rewrite Views", c)
+    val rast = passes.RewriteView.rewriteProg(ast);
+    showDebug(rast, "Rewrite Views", c)
     toBackend(c.backend).emit(rast, c)
   }
 
@@ -76,9 +78,9 @@ object Compiler {
   }
 
   def compileStringToFile(
-    prog: String,
-    c: Config,
-    out: String
+      prog: String,
+      c: Config,
+      out: String
   ): Either[String, Path] = {
 
     compileString(prog, c).map(p => {
