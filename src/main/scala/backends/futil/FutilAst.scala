3b86dce0f56677c42b7ce6ae520be5014835f334
Samuel Thomas <sgpthomas@gmail.com>
2020-07-15 16:11:07 -0700

    added 3d memories, fixed memory sizes, correctly change types in ForLoopLower

--- src/main/scala/backends/futil/FutilAst.scala
+++ src/main/scala/backends/futil/FutilAst.scala
@@ -251,6 +251,20 @@ object Stdlib {
   ): Futil.CompInst =
     Futil.CompInst("std_mem_d2", List(width, size0, size1, idxSize0, idxSize1))
 
+  def mem_d3(
+      width: Int,
+      size0: Int,
+      size1: Int,
+      size2: Int,
+      idxSize0: Int,
+      idxSize1: Int,
+      idxSize2: Int
+  ): Futil.CompInst =
+    Futil.CompInst(
+      "std_mem_d3",
+      List(width, size0, size1, size2, idxSize0, idxSize1, idxSize2)
+    )
+
   def sqrt(): Futil.CompInst =
     Futil.CompInst("std_sqrt", List())
 }
