4605c9e2fb08cc554e03e14176f62373c9d67f11
Rachit Nigam <rachit.nigam12@gmail.com>
2020-07-16 14:34:27 -0400

    improve transformer interface and transformers that try to preserve types

--- src/main/scala/passes/LowerForLoops.scala
+++ src/main/scala/passes/LowerForLoops.scala
@@ -14,7 +14,7 @@ object LowerForLoops {
 
   def rewrite(p: Prog) = ForToWhile.rewrite(p)
 
-  private final case object ForToWhile extends PartialTranformer {
+  private final case object ForToWhile extends PartialTransformer {
     case class ForEnv(map: Map[Id, Type])
         extends ScopeManager[ForEnv]
         with Tracker[Id, Type, ForEnv] {
@@ -32,7 +32,7 @@ object LowerForLoops {
     type Env = ForEnv
     val emptyEnv = ForEnv(Map())
 
-    override def myRewriteC: PF[(Command, Env), (Command, Env)] = {
+    def myRewriteC: PF[(Command, Env), (Command, Env)] = {
       case (CFor(range, pipeline, par, combine), env) => {
         if (pipeline) throw NotImplemented("Lowering pipelined for loops.")
 
@@ -44,22 +44,17 @@ object LowerForLoops {
         val itVar = EVar(it)
         itVar.typ = typ
 
-        val (init, upd, cond, nEnv) = typ match {
-          case Some(t) => {
-            val add = NumOp("+", OpConstructor.add)
-            val init = CLet(it, typ, Some(ECast(EInt(s), t)))
-            val upd = CUpdate(itVar, EBinop(add, itVar, ECast(EInt(1), t)))
-            val cond = EBinop(CmpOp("<"), itVar, ECast(EInt(e), t))
-            (init, upd, cond, env.add(it, t))
-          }
-          case None => {
-            val add = NumOp("+", OpConstructor.add)
-            val init = CLet(it, typ, Some(EInt(s)))
-            val upd = CUpdate(itVar, EBinop(add, itVar, EInt(1)))
-            val cond = EBinop(CmpOp("<"), itVar, EInt(e))
-            (init, upd, cond, env)
-          }
+        // Refuse lowering without explicit type on iterator.
+        if (typ.isDefined == false) {
+          throw NotImplemented("Cannot lower `for` loop without iterator type. Add explicit type for the iterator", it.pos)
         }
+
+        val t = typ.get
+        val add = NumOp("+", OpConstructor.add)
+        val init = CLet(it, typ, Some(ECast(EInt(s), t)))
+        val upd = CUpdate(itVar, EBinop(add, itVar, ECast(EInt(1), t)))
+        val cond = EBinop(CmpOp("<"), itVar, ECast(EInt(e), t))
+        val nEnv = env.add(it, t)
 
         // Rewrite par and combine
         val (npar, _) = rewriteC(par)(nEnv)
@@ -71,7 +66,7 @@ object LowerForLoops {
 
     /** We need to change the types of iterator variables so that
       * they match the iterators annotated type. */
-    override def myRewriteE: PF[(Expr, Env), (Expr, Env)] = {
+    def myRewriteE: PF[(Expr, Env), (Expr, Env)] = {
       case (v @ EVar(id), env) => {
         env.get(id) match {
           case Some(t) => v.typ = Some(t)
@@ -82,11 +77,10 @@ object LowerForLoops {
     }
 
     override def rewriteC(cmd: Command)(implicit env: Env) =
-      (myRewriteC.orElse(partialRewriteC))(cmd, env)
+      mergeRewriteC(myRewriteC)(cmd, env)
 
-    override def rewriteE(expr: Expr)(implicit env: Env) = {
-      super.transferType(expr, myRewriteE.orElse(partialRewriteE)(_, _))(env)
-    }
+    override def rewriteE(expr: Expr)(implicit env: Env) =
+      mergeRewriteE(myRewriteE)(expr, env)
 
   }
 
