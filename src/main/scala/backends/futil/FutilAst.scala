11c466ce523dd9c63b4c79ad3534b46a2ca249df
Rachit Nigam <rachit.nigam12@gmail.com>
2021-06-29 12:00:26 +0530

    lots of updates to clean up calyx backend

--- src/main/scala/backends/futil/FutilAst.scala
+++ src/main/scala/backends/futil/FutilAst.scala
@@ -4,8 +4,7 @@ import fuselang.common.PrettyPrint.Doc
 import fuselang.common.CompilerError._
 import Doc._
 
-object Futil {
-
+object Calyx {
   def emitCompStructure(structs: List[Structure]): Doc = {
     val (cells, connections) = structs.partition(st =>
       st match {
@@ -22,6 +21,7 @@ object Futil {
     def emit(): String = this.doc().pretty
   }
 
+  /** A variable representing the name of a component. **/
   case class CompVar(name: String) extends Emitable with Ordered[CompVar] {
     override def doc(): Doc = text(name)
     def port(port: String): CompPort = CompPort(this, port)
@@ -41,7 +41,7 @@ object Futil {
       vsep(comps.map(_.doc()))
   }
 
-  /** The statements that can appear in a `define/namespace` construct. */
+  /** The statements that can appear at the top-level. */
   sealed trait NamespaceStatement extends Emitable {
     override def doc(): Doc = this match {
       case Import(filename) => text("import") <+> quote(text(filename)) <> semi
@@ -102,14 +102,23 @@ object Futil {
 
   sealed trait Structure extends Emitable with Ordered[Structure] {
     override def doc(): Doc = this match {
-      case Cell(id, comp, true) =>
-        text("@external(1)") <+> id.doc() <+> equal <+> comp.doc() <> semi
-      case Cell(id, comp, false) =>
-        id.doc() <+> equal <+> comp.doc() <> semi
-      case Connect(src, dest, Some(guard)) =>
-        dest.doc() <+> equal <+> guard.doc() <+> text("?") <+> src.doc() <> semi
-      case Connect(src, dest, None) =>
+      case Cell(id, comp, attrs) => {
+        val attrDoc =
+          hsep(
+            attrs
+              .map({
+                case (attr, v) =>
+                  text("@") <> text(attr) <> parens(text(v.toString()))
+              })
+          ) <> (if (attrs.isEmpty) emptyDoc else space)
+
+        attrDoc <>
+          id.doc() <+> equal <+> comp.doc() <> semi
+      }
+      case Assign(src, dest, True) =>
         dest.doc() <+> equal <+> src.doc() <> semi
+      case Assign(src, dest, guard) =>
+        dest.doc() <+> equal <+> guard.doc() <+> text("?") <+> src.doc() <> semi
       case Group(id, conns, Some(delay)) =>
         text("group") <+> id.doc() <>
           angles(text("\"static\"") <> equal <> text(delay.toString())) <+>
@@ -125,7 +134,7 @@ object Futil {
           thisId.compare(thatId)
         case (Group(thisId, _, _), Group(thatId, _, _)) =>
           thisId.compare(thatId)
-        case (Connect(thisSrc, thisDest, _), Connect(thatSrc, thatDest, _)) => {
+        case (Assign(thisSrc, thisDest, _), Assign(thatSrc, thatDest, _)) => {
           if (thisSrc.compare(thatSrc) == 0) {
             thisDest.compare(thatDest)
           } else {
@@ -139,30 +148,32 @@ object Futil {
       }
     }
   }
-  case class Cell(id: CompVar, ci: CompInst, external: Boolean)
+  case class Cell(id: CompVar, ci: CompInst, attributes: List[(String, Int)])
       extends Structure
   case class Group(
       id: CompVar,
-      connections: List[Connect],
+      connections: List[Assign],
       staticDelay: Option[Int]
   ) extends Structure
+  case class Assign(src: Port, dest: Port, guard: GuardExpr = True)
+      extends Structure
+
   object Group {
     def fromStructure(
         id: CompVar,
         structure: List[Structure],
         staticDelay: Option[Int]
     ): (Group, List[Structure]) = {
-      val (connections, st) = structure.partitionMap[Connect, Structure](st =>
+      val (connections, st) = structure.partitionMap[Assign, Structure](st =>
         st match {
-          case c: Connect => Left(c)
+          case c: Assign => Left(c)
           case s => Right(s)
         }
       )
       (this(id, connections, staticDelay), st)
     }
   }
-  case class Connect(src: Port, dest: Port, guard: Option[GuardExpr] = None)
-      extends Structure
+
   case class CompInst(id: String, args: List[Int]) extends Emitable {
     override def doc(): Doc = {
       val strList = args.map((x: Int) => text(x.toString()))
@@ -176,12 +187,14 @@ object Futil {
       case And(left, right) => parens(left.doc() <+> text("&") <+> right.doc())
       case Or(left, right) => parens(left.doc() <+> text("|") <+> right.doc())
       case Not(inner) => text("!") <> inner.doc()
+      case True => emptyDoc
     }
   }
   case class Atom(item: Port) extends GuardExpr
   case class And(left: GuardExpr, right: GuardExpr) extends GuardExpr
   case class Or(left: GuardExpr, right: GuardExpr) extends GuardExpr
   case class Not(inner: GuardExpr) extends GuardExpr
+  case object True extends GuardExpr
 
   /***** control *****/
   sealed trait Control extends Emitable {
@@ -254,92 +267,38 @@ object Futil {
 
 /** Represents all of the primitives in Futil. */
 object Stdlib {
-  def register(bitwidth: Int): Futil.CompInst =
-    Futil.CompInst("std_reg", List(bitwidth))
+  def register(name: Calyx.CompVar, width: Int) =
+    Calyx.Cell(
+      name,
+      Calyx.CompInst("std_reg", List(width)),
+      List()
+    )
 
-  def iterator(bitwidth: Int, start: Int, end: Int, incr: Int): Futil.CompInst =
-    Futil.CompInst("std_iterator", List(bitwidth, start, end, incr))
+  def constant(bitwidth: Int, v: Int): Calyx.CompInst =
+    Calyx.CompInst("std_const", List(bitwidth, v))
 
-  def constant(bitwidth: Int, v: Int): Futil.CompInst =
-    Futil.CompInst("std_const", List(bitwidth, v))
-
-  def op(op: String, bitwidth: Int, is_signed: Boolean): Futil.CompInst =
-    Futil.CompInst(
-      "std_" + (if (is_signed) "s" else "") + s"$op",
+  def binop(op: String, bitwidth: Int, signed: Boolean): Calyx.CompInst =
+    Calyx.CompInst(
+      s"std_${if (signed) "s" else ""}$op",
       List(bitwidth)
     )
 
-  def slice(in: Int, out: Int): Futil.CompInst =
-    Futil.CompInst(s"std_slice", List(in, out))
+  def slice(in: Int, out: Int): Calyx.CompInst =
+    Calyx.CompInst(s"std_slice", List(in, out))
 
-  def pad(in: Int, out: Int): Futil.CompInst =
-    Futil.CompInst(s"std_pad", List(in, out))
+  def pad(in: Int, out: Int): Calyx.CompInst =
+    Calyx.CompInst(s"std_pad", List(in, out))
 
-  def identity(bitwidth: Int): Futil.CompInst =
-    Futil.CompInst(s"std_id", List(bitwidth))
-
-  def mem_d1(width: Int, size: Int, idxSize: Int): Futil.CompInst =
-    Futil.CompInst("std_mem_d1", List(width, size, idxSize))
-
-  def mem_d2(
-      width: Int,
-      size0: Int,
-      size1: Int,
-      idxSize0: Int,
-      idxSize1: Int
-  ): Futil.CompInst =
-    Futil.CompInst("std_mem_d2", List(width, size0, size1, idxSize0, idxSize1))
-
-  def mem_d3(
-      width: Int,
-      size0: Int,
-      size1: Int,
-      size2: Int,
-      idxSize0: Int,
-      idxSize1: Int,
-      idxSize2: Int
-  ): Futil.CompInst =
-    Futil.CompInst(
-      "std_mem_d3",
-      List(width, size0, size1, size2, idxSize0, idxSize1, idxSize2)
-    )
-
-  def mem_d4(
-      width: Int,
-      size0: Int,
-      size1: Int,
-      size2: Int,
-      size3: Int,
-      idxSize0: Int,
-      idxSize1: Int,
-      idxSize2: Int,
-      idxSize3: Int
-  ): Futil.CompInst =
-    Futil.CompInst(
-      "std_mem_d4",
-      List(
-        width,
-        size0,
-        size1,
-        size2,
-        size3,
-        idxSize0,
-        idxSize1,
-        idxSize2,
-        idxSize3
-      )
-    )
-
-  // Extended AST to support fixed point operations.
-  def fixed_point_op(
+  // A fixed point operator
+  def fixed_point_binop(
       op: String,
       width: Int,
       int_width: Int,
       frac_width: Int,
-      is_signed: Boolean
-  ): Futil.CompInst =
-    Futil.CompInst(
-      "std_fp_" +  (if (is_signed) "s" else "") + s"$op",
+      signed: Boolean
+  ): Calyx.CompInst =
+    Calyx.CompInst(
+      s"std_fp_${(if (signed) "s" else "")}$op",
       List(width, int_width, frac_width)
     )
 
@@ -352,10 +311,10 @@ object Stdlib {
       int_width2: Int,
       fract_width2: Int,
       out_width: Int,
-      is_signed: Boolean
-  ): Futil.CompInst =
-    Futil.CompInst(
-      "std_fp_" +  (if (is_signed) "s" else "") + s"$op" + "_dwidth",
+      signed: Boolean
+  ): Calyx.CompInst =
+    Calyx.CompInst(
+      "std_fp_" + (if (signed) "s" else "") + s"$op" + "_dwidth",
       List(
         width1,
         width2,
@@ -368,6 +327,6 @@ object Stdlib {
     )
 
   val staticTimingMap: Map[String, Int] = Map(
-    "mult" -> 3,
+    "mult" -> 3
   )
 }
